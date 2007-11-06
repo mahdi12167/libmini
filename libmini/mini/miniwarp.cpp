d1fbeab75ab473a321e03867c9c48355a8a183f4
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2007-11-06 18:09:47 +0000

    added empty class for warp kernel

---
+++ libmini/mini/miniwarp.cpp
@@ -0,0 +1,15 @@
+// (c) by Stefan Roettger
+
+#include "minibase.h"
+
+#include "miniwarp.h"
+
+// default constructor
+miniwarp::miniwarp()
+   {
+   }
+
+// destructor
+miniwarp::~miniwarp()
+   {
+   }
---
+++ libmini/mini/miniwarp.h
@@ -0,0 +1,22 @@
+// (c) by Stefan Roettger
+
+#ifndef MINIWARP_H
+#define MINIWARP_H
+
+//! warp kernel for global coordinate systems
+class miniwarp
+   {
+   public:
+
+   //! default constructor
+   miniwarp();
+
+   //! destructor
+   ~miniwarp();
+
+   protected:
+
+   private:
+   };
+
+#endif
