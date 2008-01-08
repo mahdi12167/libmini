a496043e9b2ca3c6a2b1863716e918edf6fe2088
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2008-01-08 07:07:40 +0000

    make tabify script compatible with a wider range of tcsh versions

--- libmini/mini/tabify.sh
+++ libmini/mini/tabify.sh
@@ -11,56 +11,58 @@ if ("$option" == "") then
    exit
 endif
 
-set tabify=1
+set tabify=0
 
-if ("$option" == "-u") set tabify=0
-if ("$option" == "-t") set tabify=1
+if ("$option" == "-u") set tabify=1
+if ("$option" == "-t") set tabify=2
 
-if ("$option" == "-U") set tabify=2
-if ("$option" == "-T") set tabify=3
+if ("$option" == "-U") set tabify=3
+if ("$option" == "-T") set tabify=4
+
+if ($tabify == 0) exit 1
 
 set rmctrlm='s/\\0x0d\\0x0a$/\\0x0a/'
 if ($HOSTTYPE == "i386-cygwin") set rmctrlm=''
 
-set rmspc='s/[ \t][ \t]*$//'
-set rmspctab='s/ *\t/\t/g'
+set rmspc='s/[ \\0x09]*$//'
+set rmspctab='s/ *\\0x09/\\0x09/g'
 
 if ($file == "") then
-   if ($tabify == 0) then
+   if ($tabify == 1) then
       find . \( -name \*.cpp -o -name \*.h \) -exec tabify.sh -t {} \; -exec tabify.sh -u {} \;
    endif
-   if ($tabify == 1) then
+   if ($tabify == 2) then
       find . \( -name \*.cpp -o -name \*.h \) -exec tabify.sh -u {} \; -exec tabify.sh -t {} \;
    endif
-   if ($tabify == 2) then
+   if ($tabify == 3) then
       find . \( -name \*.cpp -o -name \*.h \) -exec tabify.sh -T {} \; -exec tabify.sh -U {} \;
    endif
-   if ($tabify == 3) then
+   if ($tabify == 4) then
       find . \( -name \*.cpp -o -name \*.h \) -exec tabify.sh -U {} \; -exec tabify.sh -T {} \;
    endif
 else
-   if ($tabify == 0) then
+   if ($tabify == 1) then
       echo untabifying $file
       sed -e "$rmctrlm" -e "$rmspc" -e "$rmspctab" -e ':l\
-                                                       s/^\( *\)\t/\1    /\
+                                                       s/^\( *\)\0x09/\1    /\
                                                        t l' <$file >$file.tmp
-   endif
-   if ($tabify == 1) then
-      echo tabifying $file
-      sed -e "$rmctrlm" -e "$rmspc" -e ':l\
-                                        s/^\(\t*\)    /\1\t/\
-                                        t l' <$file >$file.tmp
    endif
    if ($tabify == 2) then
-      echo untabifying $file
-      sed -e "$rmctrlm" -e "$rmspc" -e "$rmspctab" -e ':l\
-                                                       s/^\( *\)\t/\1        /\
-                                                       t l' <$file >$file.tmp
-   endif
-   if ($tabify == 3) then
       echo tabifying $file
       sed -e "$rmctrlm" -e "$rmspc" -e ':l\
-                                        s/^\(\t*\)        /\1\t/\
+                                        s/^\(\0x09*\)    /\1\t/\
+                                        t l' <$file >$file.tmp
+   endif
+   if ($tabify == 3) then
+      echo untabifying $file
+      sed -e "$rmctrlm" -e "$rmspc" -e "$rmspctab" -e ':l\
+                                                       s/^\( *\)\0x09/\1        /\
+                                                       t l' <$file >$file.tmp
+   endif
+   if ($tabify == 4) then
+      echo tabifying $file
+      sed -e "$rmctrlm" -e "$rmspc" -e ':l\
+                                        s/^\(\t*\)        /\1\0x09/\
                                         t l' <$file >$file.tmp
    endif
    mv -f $file.tmp $file
