a8e23ff4ea174edfdb5f3384f8921315087a0947
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2008-01-11 22:47:49 +0000

    fix stupid handling of white space in .bat

--- libmini/mini/build.bat
+++ libmini/mini/build.bat
@@ -1,10 +1,10 @@
 @set BUILDDIR=%CD%
 @set PROJECT=%1%
 
-@IF '%PROJECT%' == '' ( set PROJECT="libMini" )
-@IF '%PROJECT%' == 'viewer' ( set PROJECT="libMini Viewer")
+@IF '%PROJECT%' == '' (set PROJECT="libMini")
+@IF '%PROJECT%' == 'viewer' (set PROJECT="libMini Viewer")
 
-@IF EXIST "%ProgramFiles(x86)%" ( set "PFDIR=%ProgramFiles(x86)%" ) ELSE ( set "PFDIR=%ProgramFiles%" )
+@IF EXIST "%ProgramFiles(x86)%" ( set "PFDIR=%ProgramFiles(x86)%" ) ELSE ( set "PFDIR=%ProgramFiles%")
 @call "%PFDIR%\Microsoft Visual Studio 8\Common7\Tools\vsvars32.bat"
 
 @echo building %PROJECT% with DEBUG configuration
