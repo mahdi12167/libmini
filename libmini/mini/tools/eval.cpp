30e9a984d1b9cba3e21c259658619493c0934cc4
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2008-01-28 22:11:10 +0000

    added tool to evaluate luna code

---
+++ libmini/mini/tools/eval.cpp
@@ -0,0 +1,40 @@
+#include "minibase.h"
+
+#include "miniio.h"
+
+#include "lunaparse.h"
+
+int main(int argc,char *argv[])
+   {
+   unsigned char *code;
+   unsigned int bytes;
+
+   lunaparse parser;
+   float value;
+
+   if (argc!=2)
+      {
+      printf("usage: %s <input>\n",argv[0]);
+      exit(1);
+      }
+
+   code=readfile(argv[1],&bytes);
+
+   printf("parsing code:\n----\n%s----\n",code);
+
+   parser.setLUNAcode((char *)code,bytes);
+   parser.parseLUNA();
+
+   printf("executing code:\n");
+
+   parser.getcode()->init();
+   parser.getcode()->execute();
+
+   value=parser.getcode()->popvalue();
+
+   printf("yields value: %g\n",value);
+
+   free(code);
+
+   return(0);
+   }
