8bdb1ae7b63ab2566d4a54edee520bc0bd458407
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2008-03-14 01:54:55 +0000

    added test project

---
+++ libmini/mini/tools/test.cpp
@@ -0,0 +1,24 @@
+#include "minibase.h"
+
+int main(int argc,char *argv[])
+   {
+   int i;
+
+   if (argc!=1)
+      {
+      printf("usage: %s\n",argv[0]);
+      exit(1);
+      }
+
+   printf("running test code\n");
+
+   // begin of test code section
+
+   // add test code here
+
+   // end of test code section
+
+   printf("finished test code\n");
+
+   return(0);
+   }
