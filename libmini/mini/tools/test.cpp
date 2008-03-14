1ddf3e0c266aa09349055be9fd474ca4262a88d5
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2008-03-14 23:09:26 +0000

    clean-up test

--- libmini/mini/tools/test.cpp
+++ libmini/mini/tools/test.cpp
@@ -1,9 +1,5 @@
 #include "minibase.h"
 
-#include "iostream"
-
-#include "minigeom.h"
-
 // add include files here:
 // ...
 
@@ -17,39 +13,8 @@ int main(int argc,char *argv[])
 
    printf("running test code\n\n");
 
-   // begin of test code section
-
-   const int hsnum=7;
-
-   minigeom_halfspace hs[hsnum];
-
-   hs[0]=minigeom_halfspace(miniv3d(-1,0,0),miniv3d(1,0,0));
-   hs[1]=minigeom_halfspace(miniv3d(1,0,0),miniv3d(-1,0,0));
-   hs[2]=minigeom_halfspace(miniv3d(0,-1,0),miniv3d(0,1,0));
-   hs[3]=minigeom_halfspace(miniv3d(0,1,0),miniv3d(0,-1,0));
-   hs[4]=minigeom_halfspace(miniv3d(0,0,-1),miniv3d(0,0,1));
-   hs[5]=minigeom_halfspace(miniv3d(0,0,1),miniv3d(0,0,-1));
-
-   hs[6]=minigeom_halfspace(miniv3d(0,0,0),miniv3d(-1,1,0));
-
-   minigeom_polyhedron cp;
-
-   int i,j;
-
-   for (i=0; i<hsnum; i++)
-      {
-      cp.intersect(hs[i]);
-
-      for (j=0; j<cp.getnumhalfspace(); j++)
-         std::cout << cp.gethalfspace(j).getpoint() << " " << cp.gethalfspace(j).getvector() << std::endl;
-
-      if (i<hsnum-1) std::cout << std::endl;
-      }
-
    // add test code here:
    // ...
-
-   // end of test code section
 
    printf("\nfinished test code\n");
 
