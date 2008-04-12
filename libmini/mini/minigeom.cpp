a1002541bec19f227abeaf73fcf9425f9ec43e62
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2008-04-12 06:54:22 +0000

    fixed initialization of polyhedra

--- libmini/mini/minigeom.cpp
+++ libmini/mini/minigeom.cpp
@@ -105,12 +105,12 @@ minigeom_line minigeom_halfspace::intersect(const minigeom_halfspace &halfspace)
 minigeom_polyhedron::minigeom_polyhedron()
    {
    // pre-define a closed bounding box with maximum possible size
-   half[0]=minigeom_halfspace(miniv3d(-MAXFLOAT/8,0,0),miniv3d(1,0,0));
-   half[1]=minigeom_halfspace(miniv3d(MAXFLOAT/8,0,0),miniv3d(-1,0,0));
-   half[2]=minigeom_halfspace(miniv3d(0,-MAXFLOAT/8,0),miniv3d(0,1,0));
-   half[3]=minigeom_halfspace(miniv3d(0,MAXFLOAT/8,0),miniv3d(0,-1,0));
-   half[4]=minigeom_halfspace(miniv3d(0,0,-MAXFLOAT/8),miniv3d(0,0,1));
-   half[5]=minigeom_halfspace(miniv3d(0,0,MAXFLOAT/8),miniv3d(0,0,-1));
+   half.append(minigeom_halfspace(miniv3d(-MAXFLOAT/8,0,0),miniv3d(1,0,0)));
+   half.append(minigeom_halfspace(miniv3d(MAXFLOAT/8,0,0),miniv3d(-1,0,0)));
+   half.append(minigeom_halfspace(miniv3d(0,-MAXFLOAT/8,0),miniv3d(0,1,0)));
+   half.append(minigeom_halfspace(miniv3d(0,MAXFLOAT/8,0),miniv3d(0,-1,0)));
+   half.append(minigeom_halfspace(miniv3d(0,0,-MAXFLOAT/8),miniv3d(0,0,1)));
+   half.append(minigeom_halfspace(miniv3d(0,0,MAXFLOAT/8),miniv3d(0,0,-1)));
    }
 
 // destructor
