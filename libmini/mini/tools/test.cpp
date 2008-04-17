5e555f3dfd7c6a1b32bb93336a0a05829b5763bb
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2008-04-17 20:21:49 +0000

    finished testing

--- libmini/mini/tools/test.cpp
+++ libmini/mini/tools/test.cpp
@@ -3,10 +3,6 @@
 // add include files here:
 // ...
 
-#include "iostream"
-
-#include "minimesh.h"
-
 int main(int argc,char *argv[])
    {
    if (argc!=1)
@@ -19,28 +15,6 @@ int main(int argc,char *argv[])
 
    // add test code here:
    // ...
-
-   minihedron h1=minihedron(miniv3d(0,0,0),miniv3d(1,0,0),miniv3d(0,1,0),miniv3d(0,0,1),1);
-   minihedron h2=minihedron(miniv3d(-0.75,0.25,0.25),miniv3d(-0.75,1.25,0.25),miniv3d(-0.75,0.25,1.25),miniv3d(0.25,0.25,0.25),2);
-
-   minimesh mesh;
-
-   mesh.append(h1);
-   mesh.append(h2);
-
-   std::cout << mesh;
-
-   minibsptree bspt;
-
-   bspt.insert(mesh);
-
-   minimesh sorted;
-
-   sorted=bspt.extract();
-
-   std::cout << bspt;
-
-   std::cout << sorted;
 
    printf("\nfinished test code\n");
 
