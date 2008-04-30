c282dc718eb2fb072fe947247f58177cfe1cc1f7
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2008-04-30 21:30:43 +0000

    bug fixing

--- libmini/mini/tools/test.cpp
+++ libmini/mini/tools/test.cpp
@@ -18,8 +18,9 @@ int main(int argc,char *argv[])
    // add test code here:
    // ...
 
-   minifixed4 test1(1),test2(2),test3;
+   minifixed1 test1(1),test2(2),test3;
    test1.add(test2,test3);
+   printf("1+2=%g\n",test3.get());
 
    printf("\nfinished test code\n");
 
