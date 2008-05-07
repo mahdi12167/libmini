a79fd76337decbf92e3040ebcd3a9843b254ff79
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2008-05-07 19:28:26 +0000

    improved mpfp test

--- libmini/mini/tools/test.cpp
+++ libmini/mini/tools/test.cpp
@@ -21,19 +21,25 @@ int main(int argc,char *argv[])
 
    typedef minimpfp4 minifp;
 
-   double time;
+   double time1,time2,time3;
 
-   int i,j;
+   long long int i,j;
 
    minifp test1,test2,test3;
+
+   double test;
+
+   // constants:
 
    printf("bits=%u\n",minifp::getbits());
    printf("limit=%g\n",minifp::getlimit());
 
-   time=minigettime();
+   // add speed test:
 
-   const int addmax1=100000;
-   const int addmax2=10;
+   time1=minigettime();
+
+   const long long int addmax1=100000;
+   const long long int addmax2=10;
 
    for (i=-addmax1-1; i<addmax1; i++)
       for (j=-addmax2-1; j<addmax2; j++)
@@ -47,12 +53,47 @@ int main(int argc,char *argv[])
             printf("%g+%g!=%g\n",(double)i,(double)j,test3.get());
          }
 
-   printf("%gkadds/s\n",(double)4*addmax1*addmax2/(minigettime()-time)/1000);
+   time1=minigettime()-time1;
 
-   time=minigettime();
+   // add idle test:
 
-   const int mulmax1=10000;
-   const int mulmax2=100;
+   test1=minifp(1);
+   test2=minifp(-2);
+
+   test1.add(test2,test3);
+
+   time2=minigettime();
+
+   for (i=-addmax1-1; i<addmax1; i++)
+      for (j=-addmax2-1; j<addmax2; j++)
+         if (test3.get()!=-1)
+            printf("%g+%g!=%g\n",(double)1,(double)-2,test3.get());
+
+   time2=minigettime()-time2;
+
+   printf("%gkadds/s\n",(double)4*addmax1*addmax2/(time1-time2)/1E3);
+
+   // add double test:
+
+   time3=minigettime();
+
+   for (i=-addmax1-1; i<addmax1; i++)
+      for (j=-addmax2-1; j<addmax2; j++)
+         {
+         test=(double)i+(double)j;
+         if (test==MAXFLOAT) ERRORMSG();
+         }
+
+   time3=minigettime()-time3;
+
+   printf("(%gkdadds/s)\n",(double)4*addmax1*addmax2/time3/1E3);
+
+   // mul speed test:
+
+   time1=minigettime();
+
+   const long long int mulmax1=10000;
+   const long long int mulmax2=100;
 
    for (i=-mulmax1-1; i<mulmax1; i++)
       for (j=-mulmax2-1; j<mulmax2; j++)
@@ -66,7 +107,40 @@ int main(int argc,char *argv[])
             printf("%g*%g!=%g\n",(double)i,(double)j,test3.get());
          }
 
-   printf("%gkmuls/s\n",(double)4*mulmax1*mulmax2/(minigettime()-time)/1000);
+   time1=minigettime()-time1;
+
+   // mul idle test:
+
+   test1=minifp(1);
+   test2=minifp(-2);
+
+   test1.mul(test2,test3);
+
+   time2=minigettime();
+
+   for (i=-mulmax1-1; i<mulmax1; i++)
+      for (j=-mulmax2-1; j<mulmax2; j++)
+         if (test3.get()!=-2)
+            printf("%g*%g!=%g\n",(double)-1,(double)-2,test3.get());
+
+   time2=minigettime()-time2;
+
+   printf("%gkmuls/s\n",(double)4*mulmax1*mulmax2/(time1-time2)/1E3);
+
+   // mul double test:
+
+   time3=minigettime();
+
+   for (i=-mulmax1-1; i<mulmax1; i++)
+      for (j=-mulmax2-1; j<mulmax2; j++)
+         {
+         test=(double)i*(double)j;
+         if (test==MAXFLOAT) ERRORMSG();
+         }
+
+   time3=minigettime()-time3;
+
+   printf("(%gkdmuls/s)\n",(double)4*mulmax1*mulmax2/time3/1E3);
 
    printf("\nfinished test code\n");
 
