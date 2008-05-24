b524f9c50672cf81df1edd6b3d72dc0820e72f66
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2008-05-24 06:07:08 +0000

    add tag for release 8.8

---
+++ libmini/tags/mini-8.8/minicache.h
@@ -0,0 +1,324 @@
+// (c) by Stefan Roettger
+
+#ifndef MINICACHE_H
+#define MINICACHE_H
+
+#include "miniv3d.h"
+#include "miniv4d.h"
+
+#include "miniOGL.h"
+
+#include "minitile.h"
+#include "miniwarp.h"
+
+#include "miniray.h"
+
+class minicache
+   {
+   public:
+
+   //! default constructor
+   minicache();
+
+   //! destructor
+   ~minicache();
+
+   //! attach a tileset for scene double buffering
+   void attach(minitile *terrain,
+               void (*prismedge)(float x,float y,float yf,float z,void *data)=0,
+               void (*prismcache)(int phase,float scale,float ex,float ey,float ez,void *data)=0,
+               int (*prismrender)(float *cache,int cnt,float lambda,miniwarp *warp,void *data)=0,
+               int (*prismtrigger)(int phase,void *data)=0,
+               void (*prismsync)(int id,void *data)=0,
+               void *data=0);
+
+   //! detach a tileset
+   void detach(minitile *terrain);
+
+   //! determine whether or not a tileset is displayed
+   void display(minitile *terrain,int yes=1);
+
+   //! specify per-tileset lighting
+   void setlight(minitile *terrain,float lx,float ly,float lz,float ls,float lo);
+
+   //! make cache current
+   //! required for multiple rendering windows/contexts
+   //! this should be called before updating the vertex buffers of the corresponding window
+   void makecurrent();
+
+   //! render back buffer of the cache
+   int rendercache();
+
+   //! define triangle mesh opacity
+   void setopacity(float alpha=1.0f);
+
+   //! define alpha test threshold
+   void setalphatest(float alpha=0.0f);
+
+   //! define color of semi-transparent sea
+   void setseacolor(float r=0.0f,float g=0.5f,float b=1.0f,float a=1.0f);
+
+   //! define color of prism boundary
+   void setprismcolor(float prismR=1.0f,float prismG=1.0f,float prismB=1.0f,float prismA=0.9f);
+
+   //! define optional vertex shader
+   void setvtxshader(char *vtxprog=0);
+   void setvtxshaderparams(float p1=0.0f,float p2=0.0f,float p3=0.0f,float p4=0.0f,int n=0);
+   void usevtxshader(int on=1) {USEVTXSHADER=on;}
+
+   //! define optional pixel shader
+   void setpixshader(char *fragprog=0);
+   void setpixshaderparams(float p1=0.0f,float p2=0.0f,float p3=0.0f,float p4=0.0f,int n=0);
+   void setpixshadertex(unsigned char *image,int width,int height,int components=3);
+   void setpixshadertexRGB(unsigned char *image,int width,int height);
+   void setpixshadertexRGBA(unsigned char *image,int width,int height);
+   void usepixshader(int on=1) {USEPIXSHADER=on;}
+
+   //! define optional sea shader
+   void setseashader(char *seaprog=0);
+   void setseashaderparams(float p1=0.0f,float p2=0.0f,float p3=0.0f,float p4=0.0f,int n=0);
+   void setseashadertex(unsigned char *image,int width,int height,int components=3);
+   void setseashadertexRGB(unsigned char *image,int width,int height);
+   void setseashadertexRGBA(unsigned char *image,int width,int height);
+   void useseashader(int on=0) {USESEASHADER=on;}
+
+   //! define optional sea callbacks
+   void setseacb(void (*preseacb)(void *data)=0,
+                 void (*postseacb)(void *data)=0,
+                 void *data=0);
+
+   //! get number of rendered primitives
+   int getfancnt(); // rendered triangle fans
+   int getvtxcnt(); // rendered vertices
+
+   //! get cached terrain object
+   minitile *gettile(int id);
+
+   //! get ray intersection test object
+   miniray *getray(int id);
+
+   //! configuring
+   void configure_overlap(float overlap=0.02f); // overlap of tile borders (prevents pixel flickering)
+   void configure_minsize(int minsize=33); // disable overlap for mesh size less than minsize
+   void configure_seatwosided(int seatwosided=1); // disable back face culling for the sea surface
+   void configure_seaenabletex(int seaenabletex=0); // enable texture mappping for the sea surface
+   void configure_zfight_sea(float zscale=0.99f); // shift sea towards the viewer to avoid Z-fighting
+   void configure_zfight_prisms(float zscale=0.95f); // shift prisms towards the viewer to avoid Z-fighting
+   void configure_enableray(int enableray=0); // enable ray intersection test
+   void configure_omitsea(int omitsea=0); // omit sea level ray tests
+
+   protected:
+
+   enum
+      {
+      BEGINFAN_OP=1,
+      FANVERTEX_OP=2,
+      TEXMAP_OP=3,
+      TRIGGER_OP=4
+      };
+
+   struct TERRAIN_STRUCT
+      {
+      minitile *tile;
+      float scale,lambda;
+
+      int cache_num;
+
+      unsigned char *cache1_op,*cache2_op;
+      float *cache1_arg,*cache2_arg;
+
+      int cache_size1,cache_size2;
+      int cache_maxsize;
+
+      int cache_phase;
+
+      int last_beginfan;
+
+      int fancnt1,fancnt2;
+      int vtxcnt1,vtxcnt2;
+
+      float *prism_cache1,*prism_cache2;
+
+      int prism_size1,prism_size2;
+      int prism_maxsize;
+
+      miniray *ray;
+
+      int first_fancnt;
+      int first_beginfan;
+      float first_scale;
+      int first_col;
+      int first_row;
+      int first_size;
+
+      int render_phase;
+      int render_count;
+
+      int isvisible;
+
+      float lx,ly,lz;
+      float ls,lo;
+      };
+
+   int CACHE_ID;
+   int RENDER_ID;
+
+   typedef TERRAIN_STRUCT TERRAIN_TYPE;
+
+   static minicache *CACHE;
+
+   TERRAIN_TYPE *TERRAIN;
+   int NUMTERRAIN,MAXTERRAIN;
+
+   static void cache_beginfan();
+   static void cache_fanvertex(float i,float y,float j);
+   static void cache_texmap(int m,int n,int S);
+   static void cache_prismedge(float x,float y,float yf,float z);
+   static void cache_trigger(int phase,float scale,float ex,float ey,float ez);
+   static void cache_sync(int id);
+
+   void cache(int op,float a=0.0f,float b=0.0f,float c=0.0f);
+   void cacheprismedge(float x,float y,float yf,float z);
+   void cachetrigger(int phase,float scale,float ex,float ey,float ez);
+   void cachesync(int id);
+
+   inline int rendercache(int id,int phase);
+
+   inline void rendertexmap(int m,int n,int S);
+
+   inline int rendertrigger(int phase);
+   inline int rendertrigger(int phase,float scale);
+   inline int rendertrigger();
+
+   float OPACITY,ALPHATEST;
+   float SEA_R,SEA_G,SEA_B,SEA_A;
+
+   float PRISM_R,PRISM_G,PRISM_B,PRISM_A;
+
+   char *VTXPROG;
+   int VTXDIRTY;
+
+   int VTXPROGID;
+   int USEVTXSHADER;
+
+   float VTXSHADERPAR1[8];
+   float VTXSHADERPAR2[8];
+   float VTXSHADERPAR3[8];
+   float VTXSHADERPAR4[8];
+
+   char *FRAGPROG;
+   int FRAGDIRTY;
+
+   int FRAGPROGID;
+   int USEPIXSHADER;
+
+   float PIXSHADERPAR1[8];
+   float PIXSHADERPAR2[8];
+   float PIXSHADERPAR3[8];
+   float PIXSHADERPAR4[8];
+
+   int PIXSHADERTEXID;
+   int PIXSHADERTEXWIDTH;
+   int PIXSHADERTEXHEIGHT;
+
+   char *SEAPROG;
+   int SEADIRTY;
+
+   int SEAPROGID;
+   int USESEASHADER;
+
+   float SEASHADERPAR1[8];
+   float SEASHADERPAR2[8];
+   float SEASHADERPAR3[8];
+   float SEASHADERPAR4[8];
+
+   int SEASHADERTEXID;
+   int SEASHADERTEXWIDTH;
+   int SEASHADERTEXHEIGHT;
+
+   int PRISMCACHE_VTXPROGID;
+   int PRISMCACHE_FRAGPROGID;
+
+   void (*PRISMEDGE_CALLBACK)(float x,float y,float yf,float z,void *data);
+   void (*PRISMCACHE_CALLBACK)(int phase,float scale,float ex,float ey,float ez,void *data);
+   int (*PRISMRENDER_CALLBACK)(float *cache,int cnt,float lambda,miniwarp *warp,void *data);
+   int (*PRISMTRIGGER_CALLBACK)(int phase,void *data);
+   void (*PRISMSYNC_CALLBACK)(int id,void *data);
+   void *CALLBACK_DATA;
+
+   void (*PRESEA_CB)(void *data);
+   void (*POSTSEA_CB)(void *data);
+   void *CB_DATA;
+
+   private:
+
+   float CONFIGURE_OVERLAP;
+   int CONFIGURE_MINSIZE;
+   int CONFIGURE_SEATWOSIDED;
+   int CONFIGURE_SEAENABLETEX;
+   float CONFIGURE_ZSCALE_SEA;
+   float CONFIGURE_ZSCALE_PRISMS;
+   float CONFIGURE_ENABLERAY;
+   float CONFIGURE_OMITSEA;
+
+   void initterrain(TERRAIN_TYPE *terrain);
+   void freeterrain(TERRAIN_TYPE *terrain);
+
+   int renderprisms(float *cache,int cnt,float lambda,miniwarp *warp,
+                    float pr=1.0f,float pg=1.0f,float pb=1.0f,float pa=0.9f,
+                    float lx=0.0f,float ly=0.0f,float lz=0.0f,
+                    float ls=0.0f,float lo=1.0f);
+
+   void enablevtxshader();
+   void setvtxshadertexprm(float s1,float s2,float o1,float o2,float scale);
+   void disablevtxshader();
+
+   void enablepixshader();
+   void setpixshadertexprm(float s,float o,float lx=0.0f,float ly=0.0f,float lz=0.0f,float ls=0.0f,float lo=1.0f);
+   void disablepixshader();
+
+   void enableseashader();
+   void disableseashader();
+
+   int getfancnt(int id);
+   int getvtxcnt(int id);
+
+   // OpenGL extensions:
+
+   int GLSETUP;
+
+   int GLEXT_MT;
+   int GLEXT_VP;
+   int GLEXT_FP;
+
+   void initglexts();
+
+   // Windows OpenGL extensions:
+
+   int WGLSETUP;
+
+#ifndef NOOGL
+
+#ifdef _WIN32
+
+#ifdef GL_ARB_multitexture
+   PFNGLACTIVETEXTUREARBPROC glActiveTextureARB;
+   PFNGLCLIENTACTIVETEXTUREARBPROC glClientActiveTextureARB;
+#endif
+
+#if defined(GL_ARB_vertex_program) && defined(GL_ARB_fragment_program)
+   PFNGLGENPROGRAMSARBPROC glGenProgramsARB;
+   PFNGLBINDPROGRAMARBPROC glBindProgramARB;
+   PFNGLPROGRAMSTRINGARBPROC glProgramStringARB;
+   PFNGLPROGRAMENVPARAMETER4FARBPROC glProgramEnvParameter4fARB;
+   PFNGLDELETEPROGRAMSARBPROC glDeleteProgramsARB;
+#endif
+
+#endif
+
+#endif
+
+   void initwglprocs();
+   };
+
+#endif
---
+++ libmini/tags/mini-8.8/minipoint.h
@@ -0,0 +1,236 @@
+// (c) by Stefan Roettger
+
+#ifndef MINIPOINT_H
+#define MINIPOINT_H
+
+#include "datacache.h"
+
+#include "minitile.h"
+#include "minilod.h"
+
+class minipointopts
+   {
+   public:
+
+   enum
+      {
+      OPTION_DELIMITER=1,
+      OPTION_EQ=2,
+      OPTION_TYPE=3,
+      OPTION_SIGNPOSTSIZE=4,
+      OPTION_SIGNPOSTHEIGHT=5,
+      OPTION_SIGNPOSTALPHA=6,
+      OPTION_BRICKFILE=7,
+      OPTION_BRICKSIZE=8,
+      OPTION_BRICKTURN=9,
+      OPTION_BRICKINCLINE=10,
+      OPTION_BRICKCOLOR_RED=11,
+      OPTION_BRICKCOLOR_GREEN=12,
+      OPTION_BRICKCOLOR_BLUE=13,
+      OPTION_BRICKPASSES=14
+      };
+
+   // default constructor
+   minipointopts();
+
+   // destructor
+   ~minipointopts();
+
+   int type; // type of the waypoint: none=-1 any=0 signpost=1 brick=2
+
+   // signpost parameters
+   float signpostsize;
+   float signpostheight;
+   float signpostalpha;
+
+   // brick parameters
+   char *brickfile;
+   float bricksize; // 0=default
+   float brickturn; // clockwise turning angle
+   float brickincline; // downward inclination angle
+   float brickcolor_red; // red color component
+   float brickcolor_green; // green color component
+   float brickcolor_blue; // blue color component
+   int brickpasses; // 0=default 1=striped 2-4=semi-transparent
+
+   // brick state variables
+   int brickloaded;
+   int brickindex;
+   };
+
+typedef struct
+   {
+   float x,y;
+   float elev; // waypoint elevation
+   float height; // terrain height
+
+   char *desc;
+   char *meta;
+   char *comment;
+   char *system;
+   char *latitude;
+   char *longitude;
+   char *elevation;
+   int zone,datum;
+
+   minipointopts *opts;
+
+   int number;
+   }
+minipointdata;
+
+class minipoint
+   {
+   public:
+
+   //! default constructor
+   minipoint(minitile *tile=0);
+
+   //! destructor
+   ~minipoint();
+
+   //! add waypoint
+   void add(minipointdata *point);
+
+   //! set file cache
+   void setcache(datacache *cache,char *altpath);
+
+   //! load waypoints
+   void load(char *filename,
+             float offsetlat=0.0f,float offsetlon=0.0f,
+             float scalex=1.0f,float scaley=1.0f,float scaleelev=1.0f,
+             minitile *tile=0,
+             char delimiter=';');
+
+   //! get number of waypoints
+   int getnum() {return(PNUM);}
+
+   //! get first waypoint
+   minipointdata *getfirst() {return(getpoint(0));}
+
+   //! get waypoint
+   minipointdata *getpoint(int p);
+
+   //! calculate visible waypoints
+   void calcvdata(int type1=0,int type2=0);
+
+   //! sort visible waypoints
+   void sortvdata(float x,float y,float elev,
+                  float dx,float dy,float de);
+
+   //! get visible waypoints
+   minipointdata **getvdata() {return(VPOINTS);}
+
+   //! get number of visible waypoints
+   int getvnum() {return(VNUM);}
+
+   //! get nearest waypoint
+   minipointdata *getnearest(float x,float y,float elev,int type1=0,int type2=1);
+
+   //! get squared distance to waypoint
+   float getdistance2(float x,float y,float elev,minipointdata *point);
+
+   //! render waypoints with signposts
+   void drawsignposts(float ex,float ey,float ez,
+                      float height,float range,
+                      float turn,float yon,
+                      int type1=0,int type2=1);
+
+   //! render waypoints with bricks
+   void drawbricks(float ex,float ey,float ez,
+                   float brad,float farp,
+                   float fovy,float aspect,
+                   float size,
+                   int type1=0,int type2=2);
+
+   //! set brick file name
+   void setbrick(char *filename);
+
+   //! getters:
+   float getoffsetlat() {return(OFFSETLAT);}
+   float getoffsetlon() {return(OFFSETLON);}
+   float getscalex() {return(SCALEX);}
+   float getscaley() {return(SCALEY);}
+   float getscaleelev() {return(SCALEELEV);}
+   int getzone() {return(CONFIGURE_DSTZONE);}
+   int getdatum() {return(CONFIGURE_DSTDATUM);}
+
+   //! configuring
+   void configure_srcdatum(int datum=3); // source UTM datum
+   void configure_dstzone(int zone=0); // dest UTM zone (LL=0)
+   void configure_dstdatum(int datum=3); // dest UTM datum
+   void configure_automap(int automap=0); // take mapping from first waypoint
+
+   //! configuring of signpost rendering
+   void configure_signpostalpha(float signpostalpha=0.5f); // alpha value of signposts
+
+   //! configuring of brick rendering
+   void configure_brickceiling(float brickceiling=0.0f); // elevation modulates brick color
+   void configure_brickpasses(int brickpasses=1); // render passes for brick display
+   void configure_brickalpha(float brickalpha=0.5f); // opacity of brick
+   void configure_bricklods(int bricklods=16); // number of brick LODs
+   void configure_brickstagger(float brickstagger=1.25f); // staggering of brick LODs
+   void configure_brickstripes(float brickstripes=0.0f); // offset of brick stripes
+
+   protected:
+
+   minitile *TILE;
+   int COLS,ROWS;
+
+   minipointdata **POINTS;
+   int *NUM,*MAXNUM;
+   int PNUM;
+
+   minipointdata **VPOINTS;
+   int VNUM,MAXVNUM;
+
+   datacache *CACHE;
+   char *ALTPATH;
+
+   char *getfile(char *filename,char *altpath);
+
+   char *addch(char *str,char ch);
+
+   void parsecomment(minipointdata *point);
+   void parseoption(minipointdata *point,lunascan *scanner);
+
+   private:
+
+   int TAKEN,TRANS;
+
+   float OFFSETLAT,OFFSETLON;
+   float SCALEX,SCALEY,SCALEELEV;
+
+   minilod *LODS;
+
+   char *BRICKNAME;
+
+   int CONFIGURE_SRCDATUM;
+   int CONFIGURE_DSTZONE;
+   int CONFIGURE_DSTDATUM;
+   int CONFIGURE_AUTOMAP;
+
+   float CONFIGURE_SIGNPOSTALPHA;
+
+   float CONFIGURE_BRICKCEILING;
+   int CONFIGURE_BRICKPASSES;
+   float CONFIGURE_BRICKALPHA;
+   int CONFIGURE_BRICKLODS;
+   float CONFIGURE_BRICKSTAGGER;
+   float CONFIGURE_BRICKSTRIPES;
+
+   void shellsort(minipointdata *a[],int n,
+                  float x,float y,float elev,
+                  float dx,float dy,float de);
+
+   int compare(const minipointdata *a,const minipointdata *b,
+               const float x,const float y,const float elev,
+               const float dx,const float dy,const float de);
+
+   void drawsequence(float ex,float ey,float ez,
+                     float brad,float farp,
+                     float fovy,float aspect,
+                     float size,int mpasses,int passes);
+   };
+
+#endif
