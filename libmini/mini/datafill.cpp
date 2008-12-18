da09098056724f21e7fa3546142d262860e10b68
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2008-12-18 11:08:39 +0000

    clean up fill algorithm a bit

--- libmini/mini/datafill.cpp
+++ libmini/mini/datafill.cpp
@@ -11,18 +11,26 @@ unsigned int datafill::fillin_by_regiongrowing(int radius_stop,int radius_start)
 
    unsigned int count;
 
-   count=0;
+   unsigned int sum;
 
-   for (r=radius_start; r<=radius_stop; r++) count+=fillin(r);
+   sum=0;
 
-   return(count);
+   for (r=radius_start; r<=radius_stop; r++)
+      do
+         {
+         count=fillin(r);
+         sum+=count;
+         }
+      while (count>0);
+
+   return(sum);
    }
 
 // fill-in algorithm
-// replaces no-data values by repeated region growing
+// replaces no-data values by region growing
 // smoothly extrapolates the filled-in value via partial derivatives
 // restricts the fill-in operation to concavities with a diameter of less than radius^2+1 pixels
-// a radius of zero will fill-in all no-data values
+// a radius of zero disables the concavity detection
 unsigned int datafill::fillin(int radius)
    {
    unsigned int count;
@@ -37,8 +45,6 @@ unsigned int datafill::fillin(int radius)
    int size;
    int sizex,sizey,sizez;
    int thres;
-
-   BOOLINT done;
 
    int cells;
 
@@ -65,221 +71,212 @@ unsigned int datafill::fillin(int radius)
       size=2*radius+1;
       if (size<1) size=1;
 
-      done=FALSE;
-
-      while (!done)
+      // calculate foot print size in x/y/z-direction
+      if (xsize<2)
          {
-         done=TRUE;
+         sizex=1;
+         sizey=1;
+         sizez=1;
+         }
+      else if (ysize<2)
+         {
+         sizex=size;
+         sizey=1;
+         sizez=1;
+         }
+      else if (zsize<2)
+         {
+         sizex=size;
+         sizey=size;
+         sizez=1;
+         }
+      else
+         {
+         sizex=size;
+         sizey=size;
+         sizez=size;
+         }
 
-         // calculate foot print size in x/y/z-direction
-         if (xsize<2)
-            {
-            sizex=1;
-            sizey=1;
-            sizez=1;
-            }
-         else if (ysize<2)
-            {
-            sizex=size;
-            sizey=1;
-            sizez=1;
-            }
-         else if (zsize<2)
-            {
-            sizex=size;
-            sizey=size;
-            sizez=1;
-            }
-         else
-            {
-            sizex=size;
-            sizey=size;
-            sizez=size;
-            }
+      // calculate growing threshold
+      thres=(sizex*sizey*sizez+1)/2;
 
-         // calculate growing threshold
-         thres=(sizex*sizey*sizez+1)/2;
-
-         // count no-data values if growing threshold is greater than one
-         if (thres>1)
-            {
-            // clear counting buffer
-            cnt.clear();
-
-            // search for no-data values
-            for (t=0; t<(int)tsteps; t++)
-               for (i=0; i<(int)xsize; i++)
-                  for (j=0; j<(int)ysize; j++)
-                     for (k=0; k<(int)zsize; k++)
-                        if (getval(i,j,k,t)!=nodata) cnt.setval(i,j,k,t,1);
-
-            // accumulate no-data values in x-direction
-            if (xsize>1)
-               for (t=0; t<(int)tsteps; t++)
-                  for (j=0; j<(int)ysize; j++)
-                     for (k=0; k<(int)zsize; k++)
-                        {
-                        cells=0;
-
-                        for (i=-sizex/2; i<(int)xsize; i++)
-                           {
-                           if (i-sizex/2-1>=0) cells-=ftrc(cnt.getval(i-sizex/2-1,j,k,t)+0.5f);
-                           if (i+sizex/2<(int)xsize) cells+=ftrc(cnt.getval(i+sizex/2,j,k,t)+0.5f);
-
-                           if (i>=0) tmp.setval(i,j,k,t,cells);
-                           }
-                        }
-
-            // copy counting buffer back
-            cnt.copy(&tmp);
-
-            // accumulate no-data values in y-direction
-            if (ysize>1)
-               for (t=0; t<(int)tsteps; t++)
-                  for (i=0; i<(int)xsize; i++)
-                     for (k=0; k<(int)zsize; k++)
-                        {
-                        cells=0;
-
-                        for (j=-sizey/2; j<(int)ysize; j++)
-                           {
-                           if (j-sizey/2-1>=0) cells-=ftrc(cnt.getval(i,j-sizey/2-1,k,t)+0.5f);
-                           if (j+sizey/2<(int)ysize) cells+=ftrc(cnt.getval(i,j+sizey/2,k,t)+0.5f);
-
-                           if (j>=0) tmp.setval(i,j,k,t,cells);
-                           }
-                        }
-
-            // copy counting buffer back
-            cnt.copy(&tmp);
-
-            // accumulate no-data values in z-direction
-            if (zsize>1)
-               for (t=0; t<(int)tsteps; t++)
-                  for (i=0; i<(int)xsize; i++)
-                     for (j=0; j<(int)ysize; j++)
-                        {
-                        cells=0;
-
-                        for (k=-sizez/2; k<(int)zsize; k++)
-                           {
-                           if (k-sizez/2-1>=0) cells-=ftrc(cnt.getval(i,j,k-sizez/2-1,t)+0.5f);
-                           if (k+sizez/2<(int)zsize) cells+=ftrc(cnt.getval(i,j,k+sizez/2,t)+0.5f);
-
-                           if (k>=0) tmp.setval(i,j,k,t,cells);
-                           }
-                        }
-
-            // copy counting buffer back
-            cnt.copy(&tmp);
-            }
-         else
-            {
-            // clear counting buffer
-            cnt.clear(1.0f);
-            }
+      // count no-data values if growing threshold is greater than one
+      if (thres>1)
+         {
+         // clear counting buffer
+         cnt.clear();
 
          // search for no-data values
          for (t=0; t<(int)tsteps; t++)
             for (i=0; i<(int)xsize; i++)
                for (j=0; j<(int)ysize; j++)
                   for (k=0; k<(int)zsize; k++)
-                     if (getval(i,j,k,t)==nodata)
-                        // check number of foot print cells against growing threshold
-                        if (ftrc(cnt.getval(i,j,k,t)+0.5f)>=thres)
-                           {
-                           dx=dy=dz=0.0f;
-                           dxnum=dynum=dznum=0;
+                     if (getval(i,j,k,t)!=nodata) cnt.setval(i,j,k,t,1);
 
-                           // average partial derivatives
-                           for (m=-sizex/2; m<=sizex/2; m++)
-                              for (n=-sizey/2; n<=sizey/2; n++)
-                                 for (o=-sizez/2; o<=sizez/2; o++)
-                                    if (i+m>=0 && i+m<(int)xsize && j+n>=0 && j+n<(int)ysize && k+o>=0 && k+o<(int)zsize)
-                                       {
-                                       v1=getval(i+m,j+n,k+o,t);
+         // accumulate no-data values in x-direction
+         if (xsize>1)
+            for (t=0; t<(int)tsteps; t++)
+               for (j=0; j<(int)ysize; j++)
+                  for (k=0; k<(int)zsize; k++)
+                     {
+                     cells=0;
 
-                                       if (v1!=nodata)
-                                          {
-                                          if (i+m-1>=0 && m>-sizex/2)
-                                             {
-                                             v2=getval(i+m-1,j+n,k+o,t);
+                     for (i=-sizex/2; i<(int)xsize; i++)
+                        {
+                        if (i-sizex/2-1>=0) cells-=ftrc(cnt.getval(i-sizex/2-1,j,k,t)+0.5f);
+                        if (i+sizex/2<(int)xsize) cells+=ftrc(cnt.getval(i+sizex/2,j,k,t)+0.5f);
 
-                                             if (v2!=nodata)
-                                                {
-                                                dx+=v1-v2;
-                                                dxnum++;
-                                                }
-                                             }
+                        if (i>=0) tmp.setval(i,j,k,t,cells);
+                        }
+                     }
 
-                                          if (j+n-1>=0 && n>-sizey/2)
-                                             {
-                                             v2=getval(i+m,j+n-1,k+o,t);
+         // copy counting buffer back
+         cnt.copy(&tmp);
 
-                                             if (v2!=nodata)
-                                                {
-                                                dy+=v1-v2;
-                                                dynum++;
-                                                }
-                                             }
+         // accumulate no-data values in y-direction
+         if (ysize>1)
+            for (t=0; t<(int)tsteps; t++)
+               for (i=0; i<(int)xsize; i++)
+                  for (k=0; k<(int)zsize; k++)
+                     {
+                     cells=0;
 
-                                          if (k+o-1>=0 && o>-sizez/2)
-                                             {
-                                             v2=getval(i+m,j+n,k+o-1,t);
+                     for (j=-sizey/2; j<(int)ysize; j++)
+                        {
+                        if (j-sizey/2-1>=0) cells-=ftrc(cnt.getval(i,j-sizey/2-1,k,t)+0.5f);
+                        if (j+sizey/2<(int)ysize) cells+=ftrc(cnt.getval(i,j+sizey/2,k,t)+0.5f);
 
-                                             if (v2!=nodata)
-                                                {
-                                                dz+=v1-v2;
-                                                dznum++;
-                                                }
-                                             }
-                                          }
-                                       }
+                        if (j>=0) tmp.setval(i,j,k,t,cells);
+                        }
+                     }
 
-                           if (dxnum>0) dx/=dxnum;
-                           if (dynum>0) dy/=dynum;
-                           if (dznum>0) dz/=dznum;
+         // copy counting buffer back
+         cnt.copy(&tmp);
 
-                           val=0.0f;
-                           sum=0.0f;
+         // accumulate no-data values in z-direction
+         if (zsize>1)
+            for (t=0; t<(int)tsteps; t++)
+               for (i=0; i<(int)xsize; i++)
+                  for (j=0; j<(int)ysize; j++)
+                     {
+                     cells=0;
 
-                           // extrapolate partial derivatives
-                           for (m=-sizex/2; m<=sizex/2; m++)
-                              for (n=-sizey/2; n<=sizey/2; n++)
-                                 for (o=-sizez/2; o<=sizez/2; o++)
-                                    if (i+m>=0 && i+m<(int)xsize && j+n>=0 && j+n<(int)ysize && k+o>=0 && k+o<(int)zsize)
-                                       {
-                                       v1=getval(i+m,j+n,k+o,t);
+                     for (k=-sizez/2; k<(int)zsize; k++)
+                        {
+                        if (k-sizez/2-1>=0) cells-=ftrc(cnt.getval(i,j,k-sizez/2-1,t)+0.5f);
+                        if (k+sizez/2<(int)zsize) cells+=ftrc(cnt.getval(i,j,k+sizez/2,t)+0.5f);
 
-                                       if (v1!=nodata)
-                                          {
-                                          v2=v1-m*dx-n*dy-o*dz;
-                                          weight=m*m+n*n+o*o;
+                        if (k>=0) tmp.setval(i,j,k,t,cells);
+                        }
+                     }
 
-                                          if (weight>0.0f)
-                                             {
-                                             val+=v2/weight;
-                                             sum+=1.0f/weight;
-                                             }
-                                          }
-                                       }
-
-                           // fill-in extrapolated value
-                           if (sum>0.0f)
-                              {
-                              val/=sum;
-                              if ((val-bias)/scaling==nodata) val+=scaling;
-
-                              buf.setval(i,j,k,t,val);
-                              count++;
-
-                              done=FALSE;
-                              }
-                           }
-
-         // copy working buffer back
-         copy(&buf);
+         // copy counting buffer back
+         cnt.copy(&tmp);
          }
+      else
+         {
+         // clear counting buffer
+         cnt.clear(1.0f);
+         }
+
+      // search for no-data values
+      for (t=0; t<(int)tsteps; t++)
+         for (i=0; i<(int)xsize; i++)
+            for (j=0; j<(int)ysize; j++)
+               for (k=0; k<(int)zsize; k++)
+                  if (getval(i,j,k,t)==nodata)
+                     // check number of foot print cells against growing threshold
+                     if (ftrc(cnt.getval(i,j,k,t)+0.5f)>=thres)
+                        {
+                        dx=dy=dz=0.0f;
+                        dxnum=dynum=dznum=0;
+
+                        // average partial derivatives
+                        for (m=-sizex/2; m<=sizex/2; m++)
+                           for (n=-sizey/2; n<=sizey/2; n++)
+                              for (o=-sizez/2; o<=sizez/2; o++)
+                                 if (i+m>=0 && i+m<(int)xsize && j+n>=0 && j+n<(int)ysize && k+o>=0 && k+o<(int)zsize)
+                                    {
+                                    v1=getval(i+m,j+n,k+o,t);
+
+                                    if (v1!=nodata)
+                                       {
+                                       if (i+m-1>=0 && m>-sizex/2)
+                                          {
+                                          v2=getval(i+m-1,j+n,k+o,t);
+
+                                          if (v2!=nodata)
+                                             {
+                                             dx+=v1-v2;
+                                             dxnum++;
+                                             }
+                                          }
+
+                                       if (j+n-1>=0 && n>-sizey/2)
+                                          {
+                                          v2=getval(i+m,j+n-1,k+o,t);
+
+                                          if (v2!=nodata)
+                                             {
+                                             dy+=v1-v2;
+                                             dynum++;
+                                             }
+                                          }
+
+                                       if (k+o-1>=0 && o>-sizez/2)
+                                          {
+                                          v2=getval(i+m,j+n,k+o-1,t);
+
+                                          if (v2!=nodata)
+                                             {
+                                             dz+=v1-v2;
+                                             dznum++;
+                                             }
+                                          }
+                                       }
+                                    }
+
+                        if (dxnum>0) dx/=dxnum;
+                        if (dynum>0) dy/=dynum;
+                        if (dznum>0) dz/=dznum;
+
+                        val=0.0f;
+                        sum=0.0f;
+
+                        // extrapolate partial derivatives
+                        for (m=-sizex/2; m<=sizex/2; m++)
+                           for (n=-sizey/2; n<=sizey/2; n++)
+                              for (o=-sizez/2; o<=sizez/2; o++)
+                                 if (i+m>=0 && i+m<(int)xsize && j+n>=0 && j+n<(int)ysize && k+o>=0 && k+o<(int)zsize)
+                                    {
+                                    v1=getval(i+m,j+n,k+o,t);
+
+                                    if (v1!=nodata)
+                                       {
+                                       v2=v1-m*dx-n*dy-o*dz;
+                                       weight=m*m+n*n+o*o;
+
+                                       if (weight>0.0f)
+                                          {
+                                          val+=v2/weight;
+                                          sum+=1.0f/weight;
+                                          }
+                                       }
+                                    }
+
+                        // fill-in extrapolated value
+                        if (sum>0.0f)
+                           {
+                           val/=sum;
+                           if ((val-bias)/scaling==nodata) val+=scaling;
+
+                           buf.setval(i,j,k,t,val);
+                           count++;
+                           }
+                        }
+
+      // copy working buffer back
+      copy(&buf);
 
       // free working buffer
       buf.release();
