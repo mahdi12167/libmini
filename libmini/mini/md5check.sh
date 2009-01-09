5055898520f18d4f6f11c7a1e1a89cff4c56e029
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2009-01-09 14:19:05 +0000

    make md5 check more verbose

--- libmini/mini/md5check.sh
+++ libmini/mini/md5check.sh
@@ -11,7 +11,7 @@ endif
 if ("$dir" == "") then
    echo "usage: md5check.sh [-t] <dir>"
    echo " checks the integrity of a directory <dir>"
-   echo " creates a md5 file list of the directory"
+   echo " creates a md5 file list of the directory (in <dir>.md5)"
    echo " verifies that the md5 of each file matches with the md5 list"
    echo " use the -t switch to ignore mismatches due to an updated time stamp"
    echo " uses the md5sum tool of the GNU coreutils"
@@ -25,17 +25,22 @@ else
 endif
 
 if (! -e $md5) then
+   echo creating md5 file list
    find $dir \( -type f -a \! -name .md5 -a \! -path \*.svn\* \) -exec md5sum {} \; > $md5
 else
+   echo checking integrity
    if ($stamp) then
       find $dir \( -type f -a \! -newer $md5 -a \! -name .md5 -a \! -path \*.svn\* \) -exec egrep \{\}\$ $md5 \; > $md5.tmp
 
       md5sum --check --status $md5.tmp
 
       if ($status) then
+         echo detected md5 mismatch
          md5sum --check $md5.tmp | grep FAILED
          rm $md5.tmp
          exit 1
+      else
+         echo verified md5 file list
       endif
 
       find $dir \( -type f -a -newer $md5 -a \! -name .md5 -a \! -path \*.svn\* \) -exec md5sum {} \; >> $md5.tmp
@@ -45,8 +50,11 @@ else
       md5sum --check --status $md5
 
       if ($status) then
+         echo detected md5 mismatch
          md5sum --check $md5 | grep FAILED
          exit 1
+      else
+         echo verified md5 file list
       endif
    endif
 endif
