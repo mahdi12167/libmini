90126f759cb684d74dd4d553b9034acaa252bb8f
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2009-01-11 17:46:21 +0000

    clean up build script that gets the deps

--- libmini/deps/build.sh
+++ libmini/deps/build.sh
@@ -1,16 +1,25 @@
 #!/bin/tcsh -f
 
-cvs -d:pserver:anonymous@cimg.cvs.sourceforge.net:/cvsroot/cimg login
-cvs -z3 -d:pserver:anonymous@cimg.cvs.sourceforge.net:/cvsroot/cimg co -P CImg
+# update curl:
 
-foreach file (CImg.h plugins/greycstoration.h)
-   cp -f CImg/$file greycstoration/$file
-   ../mini/tabify.sh -u greycstoration/$file
-end
+cvs -d:pserver:anonymous@cool.haxx.se:/cvsroot/curl login
+cvs -d:pserver:anonymous@cool.haxx.se:/cvsroot/curl co curl
 
-svn up squish-head
+# update squish:
+
+svn co http://libsquish.googlecode.com/svn/trunk squish-head
 
 foreach file (squish-head/*.cpp squish-head/*.h squish-head/*.inl)
    cp -f $file $file:s/-head//
    ../mini/tabify.sh -u $file:s/-head//
+end
+
+# update GREYCstoration:
+
+cvs -d:pserver:anonymous@cimg.cvs.sourceforge.net:/cvsroot/cimg login
+cvs -d:pserver:anonymous@cimg.cvs.sourceforge.net:/cvsroot/cimg co CImg
+
+foreach file (CImg.h plugins/greycstoration.h)
+   cp -f CImg/$file greycstoration/$file
+   ../mini/tabify.sh -u greycstoration/$file
 end
