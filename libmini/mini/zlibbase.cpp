a05d99925aaa604000325cbcf1463d9ca4c55f7b
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2009-03-14 02:29:33 +0000

    implemented zlib decompression

--- libmini/mini/zlibbase.cpp
+++ libmini/mini/zlibbase.cpp
@@ -16,39 +16,62 @@ namespace zlibbase {
 // decompress with ZLIB
 unsigned char *decompressZLIB(unsigned char *chunk,unsigned int chunklen,unsigned int *bytes)
    {
-   //!! todo
+   int i;
+
+   Bytef *mem;
+   uLongf len;
+
+   int status;
+
+   len=4*chunklen;
+
+   for (i=0; i<20; i++)
+      {
+      if ((mem=(Bytef *)malloc(len))==NULL) break;
+
+      if ((status=uncompress(mem,&len,chunk,chunklen))==Z_OK)
+         {
+         if ((mem=(Bytef *)realloc(mem,len))==NULL) break;
+         *bytes=len;
+         return(mem);
+         }
+
+      if (status!=Z_BUF_ERROR) break;
+
+      free(mem);
+      len*=2;
+      }
+
    return(NULL);
    }
 
 // compress with ZLIB
 void compressZLIB(unsigned char *data,unsigned int bytes,unsigned char **chunk,unsigned int *chunklen,int level)
    {
+   int i;
+
    Bytef *mem;
    uLongf len;
 
+   int status;
+
    len=bytes+bytes/100+12;
-   if ((mem=(Bytef *)malloc(len))==NULL) ERRORMSG();
 
-   if (compress2(mem,&len,data,bytes,level)==Z_OK)
+   for (i=0; i<20; i++)
       {
-      if (((*chunk)=(unsigned char *)realloc(mem,len))==NULL) ERRORMSG();
-      *chunklen=len;
-      return;
-      }
-   else
-      {
-      free(mem);
+      if ((mem=(Bytef *)malloc(len))==NULL) break;
 
-      len=2*bytes+12;
-      if ((mem=(Bytef *)malloc(len))==NULL) ERRORMSG();
-
-      if (compress2(mem,&len,data,bytes,level)==Z_OK)
+      if ((status=compress2(mem,&len,data,bytes,level))==Z_OK)
          {
-         if (((*chunk)=(unsigned char *)realloc(mem,len))==NULL) ERRORMSG();
+         if (((*chunk)=(unsigned char *)realloc(mem,len))==NULL) break;
          *chunklen=len;
          return;
          }
-      else free(mem);
+
+      if (status!=Z_BUF_ERROR) break;
+
+      free(mem);
+      len*=2;
       }
 
    *chunk=NULL;
