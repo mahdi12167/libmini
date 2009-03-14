046b75af90275a656d40164b6bbb5283b3ca0428
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2009-03-14 01:16:03 +0000

    implement zlib compression

--- libmini/mini/zlibbase.cpp
+++ libmini/mini/zlibbase.cpp
@@ -23,7 +23,34 @@ unsigned char *decompressZLIB(unsigned char *chunk,unsigned int chunklen,unsigne
 // compress with ZLIB
 void compressZLIB(unsigned char *data,unsigned int bytes,unsigned char **chunk,unsigned int *chunklen,int level)
    {
-   //!! todo
+   Bytef *mem;
+   uLongf len;
+
+   len=bytes+bytes/100+12;
+   if ((mem=(Bytef *)malloc(len))==NULL) ERRORMSG();
+
+   if (compress2(mem,&len,data,bytes,level)==Z_OK)
+      {
+      if (((*chunk)=(unsigned char *)realloc(mem,len))==NULL) ERRORMSG();
+      *chunklen=len;
+      return;
+      }
+   else
+      {
+      free(mem);
+
+      len=2*bytes+12;
+      if ((mem=(Bytef *)malloc(len))==NULL) ERRORMSG();
+
+      if (compress2(mem,&len,data,bytes,level)==Z_OK)
+         {
+         if (((*chunk)=(unsigned char *)realloc(mem,len))==NULL) ERRORMSG();
+         *chunklen=len;
+         return;
+         }
+      else free(mem);
+      }
+
    *chunk=NULL;
    *chunklen=0;
    }
