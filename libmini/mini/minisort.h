49d85b0b1e4008af28ee1c4fe2bb213e772c9dde
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2009-03-18 01:33:19 +0000

    add sorting template for dynamic array

--- libmini/mini/minisort.h
+++ libmini/mini/minisort.h
@@ -3,13 +3,44 @@
 
 #include "minibase.h"
 
+#include "minidyna.h"
+
 namespace minisort {
 
-// Shellsort as proposed by Robert Sedgewick in "Algorithms"
+// Shellsort as proposed by Robert Sedgewick in "Algorithms":
+
+// templated version for indexed arrays
 template <class Item>
-void shellsort(Item *a[],const int n)
+void shellsort(Item a[],const unsigned int n)
    {
-   int i,j,h;
+   unsigned int i,j,h;
+
+   Item v;
+
+   for (h=1; h<=(n-1)/9; h=3*h+1);
+
+   while (h>0)
+      {
+      for (i=h; i<n; i++)
+         {
+         j=i;
+         v=a[i];
+         while (j>=h && v<a[j-h])
+            {
+            a[j]=a[j-h];
+            j-=h;
+            }
+         a[j]=v;
+         }
+      h/=3;
+      }
+   }
+
+// templated version for indexed pointer arrays
+template <class Item>
+void shellsort(Item *a[],const unsigned int n)
+   {
+   unsigned int i,j,h;
 
    Item *v;
 
@@ -22,6 +53,37 @@ void shellsort(Item *a[],const int n)
          j=i;
          v=a[i];
          while (j>=h && (*v)<(*a[j-h]))
+            {
+            a[j]=a[j-h];
+            j-=h;
+            }
+         a[j]=v;
+         }
+      h/=3;
+      }
+   }
+
+// templated version for minidyna arrays
+template <class Item>
+void shellsort(minidyna<Item> &a)
+   {
+   unsigned int i,j,h;
+
+   unsigned int n;
+
+   Item v;
+
+   n=a.getsize();
+
+   for (h=1; h<=(n-1)/9; h=3*h+1);
+
+   while (h>0)
+      {
+      for (i=h; i<n; i++)
+         {
+         j=i;
+         v=a[i];
+         while (j>=h && v<a[j-h])
             {
             a[j]=a[j-h];
             j-=h;
