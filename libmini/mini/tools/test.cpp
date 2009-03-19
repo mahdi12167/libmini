7b179b7d843fe27a21c47bbb7a809671ac374b46
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2009-03-19 01:54:55 +0000

    testing checkpoint

--- libmini/mini/tools/test.cpp
+++ libmini/mini/tools/test.cpp
@@ -87,6 +87,39 @@ void keyboardfunc(unsigned char key,int x,int y)
 
 #ifdef MESHTEST
 
+inline miniv3d intersect(const miniv3d &p,const miniv3d &d,const miniv3d &o,const miniv3d &n)
+   {
+   double lambda;
+
+   lambda=n*d;
+   if (lambda!=0.0) lambda=n*(o-p)/lambda;
+
+   return(p+lambda*d);
+   }
+
+inline miniv3d reproject(const miniv3d &p,const miniv3d &d,const miniv3d &v1,const miniv3d &v2,const miniv3d &v3,const miniv3d &v4)
+   {
+   miniv3d m,n;
+
+   m=(v1+v2+v3+v4)/4.0;
+   n=(v1-v2)/(v3-v2)+(v3-v4)/(v1-v4)+(v4-v1)/(v2-v1)+(v2-v3)/(v4-v3);
+   n.normalize();
+
+   return(intersect(p,d,m,n));
+   }
+
+inline double coplanar(const miniv3d &v1,const miniv3d &v2,const miniv3d &v3,const miniv3d &v4)
+   {
+   miniv3d n1,n2;
+
+   n1=(v2-v1)/(v3-v1);
+   n1.normalize();
+   n2=(v3-v4)/(v2-v4);
+   n2.normalize();
+
+   return(FABS(n1*n2));
+   }
+
 void addhex(miniv3d crd1,miniv3d crd2,
             minicoord crd,BOOLINT flip,
             unsigned int slot,unsigned int brickid,
@@ -96,7 +129,11 @@ void addhex(miniv3d crd1,miniv3d crd2,
 
    minicoord vtx[8];
 
+#if 0
    for (i=0; i<8; i++) vtx[i]=crd;
+#else
+   for (i=0; i<8; i++) vtx[i]=miniv3d(0.0);
+#endif
 
    vtx[0].vec+=miniv4d(crd1.x,crd1.y,crd1.z);
    vtx[1].vec+=miniv4d(crd1.x,crd2.y,crd1.z);
@@ -110,23 +147,79 @@ void addhex(miniv3d crd1,miniv3d crd2,
 
 #if 0
    for (i=0; i<8; i++) vtx[i].convert2(minicoord::MINICOORD_ECEF);
-#else
-if (!flip)
-   {
-   vtx[5].vec.z-=0.9*(vtx[5].vec.z-vtx[1].vec.z);
-   vtx[7].vec.z-=0.9*(vtx[7].vec.z-vtx[3].vec.z);
+#endif
 
-   vtx[0].vec.z-=0.9*(vtx[4].vec.z-vtx[0].vec.z);
-   vtx[2].vec.z-=0.9*(vtx[6].vec.z-vtx[2].vec.z);
-   }
-else
-   {
-   vtx[4].vec.z-=0.9*(vtx[4].vec.z-vtx[0].vec.z);
-   vtx[6].vec.z-=0.9*(vtx[6].vec.z-vtx[2].vec.z);
+#if 1
+   if (!flip)
+      {
+      vtx[5].vec.z-=0.9*(vtx[5].vec.z-vtx[1].vec.z);
+      vtx[7].vec.z-=0.9*(vtx[7].vec.z-vtx[3].vec.z);
 
-   vtx[1].vec.z-=0.9*(vtx[5].vec.z-vtx[1].vec.z);
-   vtx[3].vec.z-=0.9*(vtx[7].vec.z-vtx[3].vec.z);
-   }
+      vtx[0].vec.z-=0.9*(vtx[4].vec.z-vtx[0].vec.z);
+      vtx[2].vec.z-=0.9*(vtx[6].vec.z-vtx[2].vec.z);
+      }
+   else
+      {
+      vtx[4].vec.z-=0.9*(vtx[4].vec.z-vtx[0].vec.z);
+      vtx[6].vec.z-=0.9*(vtx[6].vec.z-vtx[2].vec.z);
+
+      vtx[1].vec.z-=0.9*(vtx[5].vec.z-vtx[1].vec.z);
+      vtx[3].vec.z-=0.9*(vtx[7].vec.z-vtx[3].vec.z);
+      }
+#endif
+
+#if 1
+   vtx[0].vec=reproject(vtx[0].vec,vtx[0].vec-vtx[4].vec,vtx[0].vec,vtx[1].vec,vtx[2].vec,vtx[3].vec);
+   vtx[1].vec=reproject(vtx[1].vec,vtx[1].vec-vtx[5].vec,vtx[0].vec,vtx[1].vec,vtx[2].vec,vtx[3].vec);
+   vtx[2].vec=reproject(vtx[2].vec,vtx[2].vec-vtx[6].vec,vtx[0].vec,vtx[1].vec,vtx[2].vec,vtx[3].vec);
+   vtx[3].vec=reproject(vtx[3].vec,vtx[3].vec-vtx[7].vec,vtx[0].vec,vtx[1].vec,vtx[2].vec,vtx[3].vec);
+
+   vtx[4].vec=reproject(vtx[4].vec,vtx[4].vec-vtx[0].vec,vtx[4].vec,vtx[5].vec,vtx[6].vec,vtx[7].vec);
+   vtx[5].vec=reproject(vtx[5].vec,vtx[5].vec-vtx[1].vec,vtx[4].vec,vtx[5].vec,vtx[6].vec,vtx[7].vec);
+   vtx[6].vec=reproject(vtx[6].vec,vtx[6].vec-vtx[2].vec,vtx[4].vec,vtx[5].vec,vtx[6].vec,vtx[7].vec);
+   vtx[7].vec=reproject(vtx[7].vec,vtx[7].vec-vtx[3].vec,vtx[4].vec,vtx[5].vec,vtx[6].vec,vtx[7].vec);
+#endif
+
+#if 0
+   miniv3d up;
+   miniv3d lift1,lift2;
+
+   up=vtx[4].vec-vtx[0].vec+vtx[5].vec-vtx[1].vec+vtx[6].vec-vtx[2].vec+vtx[7].vec-vtx[3].vec;
+   up.normalize();
+
+   if (!flip)
+      {
+      lift1=vtx[4].vec+vtx[6].vec-vtx[5].vec-vtx[7].vec;
+      lift1.normalize();
+      lift2=vtx[1].vec+vtx[3].vec-vtx[0].vec-vtx[2].vec;
+      lift2.normalize();
+      }
+   else
+      {
+      lift1=vtx[5].vec+vtx[7].vec-vtx[4].vec-vtx[6].vec;
+      lift1.normalize();
+      lift2=vtx[0].vec+vtx[2].vec-vtx[1].vec-vtx[3].vec;
+      lift2.normalize();
+      }
+
+   printf("lift1=%g lift2=%g\n",lift1*up,lift2*up);
+#endif
+
+#if 0
+   if (!flip)
+      {
+      printf("planar1=%g\n",coplanar(vtx[0].vec,vtx[3].vec,vtx[4].vec,vtx[7].vec));
+      printf("planar2=%g\n",coplanar(vtx[2].vec,vtx[3].vec,vtx[6].vec,vtx[7].vec));
+      printf("planar3=%g\n",coplanar(vtx[2].vec,vtx[1].vec,vtx[6].vec,vtx[5].vec));
+      printf("planar4=%g\n",coplanar(vtx[0].vec,vtx[1].vec,vtx[4].vec,vtx[5].vec));
+      }
+   else
+      {
+      printf("planar1=%g\n",coplanar(vtx[3].vec,vtx[0].vec,vtx[7].vec,vtx[4].vec));
+      printf("planar2=%g\n",coplanar(vtx[3].vec,vtx[2].vec,vtx[7].vec,vtx[6].vec));
+      printf("planar3=%g\n",coplanar(vtx[1].vec,vtx[2].vec,vtx[5].vec,vtx[6].vec));
+      printf("planar4=%g\n",coplanar(vtx[1].vec,vtx[0].vec,vtx[5].vec,vtx[4].vec));
+      }
 #endif
 
    if (!flip)
@@ -201,7 +294,7 @@ int main(int argc,char *argv[])
    double gfny=2361214.0;
    double gfnh=1000.0;
    int gfnz=4;
-   double brick=1000000.0;
+   double brick=1.0;
    minicoord crd(gfnx,gfny,gfnh,minicoord::MINICOORD_UTM,gfnz,minicoord::MINICOORD_DATUM_WGS84);
    addhex(miniv3d(-1.0,-1.0,0.0)*brick,miniv3d(0.0,0.0,1.0)*brick,crd,FALSE,1,0,&mesh);
    addhex(miniv3d(0.0,-1.0,0.0)*brick,miniv3d(1.0,0.0,1.0)*brick,crd,TRUE,1,1,&mesh);
@@ -218,6 +311,8 @@ int main(int argc,char *argv[])
 
    std::cout << "in=" << mesh.getsize() << " out=" << unsorted.getsize() << std::endl;
    std::cout << "vol_in=" << mesh.getvolume() << " vol_out=" << unsorted.getvolume() << std::endl;
+
+   for (unsigned int i=0; i<unsorted.getsize(); i++) printf("vol=%g\n",unsorted[i].getvolume());
 
 #endif
 
