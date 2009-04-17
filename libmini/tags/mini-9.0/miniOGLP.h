eb955a6da480cd617f860fd3c9033b00a429fe96
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2009-04-17 08:55:09 +0000

    tag libMini version 9.0

---
+++ libmini/tags/mini-9.0/miniOGLP.h
@@ -0,0 +1,80 @@
+// (c) by Stefan Roettger
+
+#include "minibase.h"
+
+#include "miniOGL.h"
+
+namespace miniOGL {
+
+int CONFIGURE_GENERATEMM=0;
+int CONFIGURE_COMPRESSION=1;
+int CONFIGURE_DEPTH=24;
+float CONFIGURE_GAMMA=1.0f;
+float CONFIGURE_LEVEL=0.0f;
+
+#ifndef NOOGL
+
+GLboolean depth_test,cull_face,color_writemask[4],depth_writemask,blending;
+GLint depth_func,front_face,cull_face_mode,blend_src,blend_dst;
+
+BOOLINT maxblendeq;
+
+BOOLINT glrndr_sgi;
+BOOLINT glrndr_nvidia;
+BOOLINT glrndr_ati;
+
+BOOLINT glext_mm;
+BOOLINT glext_tec;
+BOOLINT glext_tfa;
+BOOLINT glext_t3D;
+BOOLINT glext_tc;
+BOOLINT glext_ts3;
+BOOLINT glext_tgm;
+BOOLINT glext_np2;
+BOOLINT glext_dt;
+BOOLINT glext_tr;
+BOOLINT glext_mt;
+BOOLINT glext_vp;
+BOOLINT glext_fp;
+BOOLINT glext_gpp;
+
+#ifdef _WIN32
+
+#ifdef GL_EXT_blend_minmax
+PFNGLBLENDEQUATIONEXTPROC glBlendEquation=NULL;
+#endif
+
+#ifdef GL_EXT_texture3D
+PFNGLTEXIMAGE3DEXTPROC glTexImage3D=NULL;
+#endif
+
+#ifdef GL_ARB_texture_compression
+PFNGLCOMPRESSEDTEXIMAGE2DARBPROC glCompressedTexImage2DARB=NULL;
+PFNGLGETCOMPRESSEDTEXIMAGEARBPROC glGetCompressedTexImageARB=NULL;
+#endif
+
+#ifdef GL_ARB_multitexture
+PFNGLACTIVETEXTUREARBPROC glActiveTextureARB=NULL;
+PFNGLCLIENTACTIVETEXTUREARBPROC glClientActiveTextureARB=NULL;
+PFNGLMULTITEXCOORD3FARBPROC glMultiTexCoord3fARB=NULL;
+#endif
+
+#if defined(GL_ARB_vertex_program) && defined(GL_ARB_fragment_program)
+PFNGLGENPROGRAMSARBPROC glGenProgramsARB=NULL;
+PFNGLBINDPROGRAMARBPROC glBindProgramARB=NULL;
+PFNGLPROGRAMSTRINGARBPROC glProgramStringARB=NULL;
+PFNGLPROGRAMENVPARAMETER4FARBPROC glProgramEnvParameter4fARB=NULL;
+PFNGLDELETEPROGRAMSARBPROC glDeleteProgramsARB=NULL;
+#endif
+
+#ifdef GL_EXT_gpu_program_parameters
+PFNGLPROGRAMENVPARAMETERS4FVEXTPROC glProgramEnvParameters4fvEXT=NULL;
+#endif
+
+#endif
+
+int fancnt,vtxcnt;
+
+#endif
+
+}
