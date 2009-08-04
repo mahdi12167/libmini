f07fa1fc7dd491bc13011b31772148d13f349358
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2009-08-04 14:42:50 +0000

    add sorting template

--- libmini/mini/minisort.h
+++ libmini/mini/minisort.h
@@ -19,6 +19,8 @@ void shellsort(Item a[],const unsigned int n)
 
    Item v;
 
+   if (n==0) return;
+
    for (h=1; h<=(n-1)/9; h=3*h+1);
 
    while (h>0)
@@ -45,6 +47,8 @@ void shellsort(Item *a[],const unsigned int n)
    unsigned int i,j,h;
 
    Item *v;
+
+   if (n==0) return;
 
    for (h=1; h<=(n-1)/9; h=3*h+1);
 
@@ -77,6 +81,8 @@ void shellsort(minidyna<Item> &a)
 
    n=a.getsize();
 
+   if (n==0) return;
+
    for (h=1; h<=(n-1)/9; h=3*h+1);
 
    while (h>0)
@@ -86,6 +92,39 @@ void shellsort(minidyna<Item> &a)
          j=i;
          v=a[i];
          while (j>=h && v<a[j-h])
+            {
+            a[j]=a[j-h];
+            j-=h;
+            }
+         a[j]=v;
+         }
+      h/=3;
+      }
+   }
+
+// templated version for dynamic pointer arrays
+template <class Item>
+void shellsort(minidyna<Item *> &a)
+   {
+   unsigned int i,j,h;
+
+   unsigned int n;
+
+   Item *v;
+
+   n=a.getsize();
+
+   if (n==0) return;
+
+   for (h=1; h<=(n-1)/9; h=3*h+1);
+
+   while (h>0)
+      {
+      for (i=h; i<n; i++)
+         {
+         j=i;
+         v=a[i];
+         while (j>=h && (*v)<(*a[j-h]))
             {
             a[j]=a[j-h];
             j-=h;
