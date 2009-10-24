00214d7141d1d06db06d2b87fa9b3397918e6ca6
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2009-10-24 09:52:01 +0000

    tag v9.5

---
+++ libmini/tags/mini-9.5/minishader.h
@@ -0,0 +1,68 @@
+// (c) by Stefan Roettger
+
+#ifndef MINISHADER_H
+#define MINISHADER_H
+
+#include "minicache.h"
+
+class minishader
+   {
+   public:
+
+   static void setVISshader(minicache *cache,
+                            float scale,float exaggeration,
+                            float fogstart,float fogend,
+                            float fogdensity,
+                            float fogcolor[3],
+                            float bathystart,float bathyend,
+                            float contours,
+                            float sealevel,float seabottom,
+                            float seacolor[3],
+                            float seatrans,float bottomtrans,
+                            float bottomcolor[3],
+                            float seamodulate);
+
+   static void setVISbathymap(unsigned char *bathymap,
+                              int bathywidth,int bathyheight,int bathycomps);
+
+   static void setNPRshader(minicache *cache,
+                            float scale,float exaggeration,
+                            float fogstart,float fogend,
+                            float fogdensity,
+                            float fogcolor[3],
+                            float bathystart,float bathyend,
+                            float contours,
+                            float fadefactor,
+                            float sealevel,
+                            float seacolor[3],float seatrans,
+                            float seagray);
+
+   static void setNPRbathymap(unsigned char *bathymap,
+                              int bathywidth,int bathyheight,int bathycomps);
+
+   static void setcontourmode(int mode);
+   static void setdetailtexmode(int mode,float alpha=1.0f,int mask=0);
+   static void setseamode(int mode);
+
+   static void unsetshaders(minicache *cache);
+
+   private:
+
+   static unsigned char *VISBATHYMAP;
+   static int VISBATHYWIDTH,VISBATHYHEIGHT,VISBATHYCOMPS;
+   static int VISBATHYMOD;
+
+   static unsigned char *NPRBATHYMAP;
+   static int NPRBATHYWIDTH,NPRBATHYHEIGHT,NPRBATHYCOMPS;
+   static int NPRBATHYMOD;
+
+   static int CONTOURMODE;
+
+   static int DETAILTEXMODE;
+   static float DETAILTEXALPHA;
+   static int DETAILTEXMASK;
+
+   static int SEAMODE;
+   };
+
+#endif
---
+++ libmini/tags/mini-9.5/tools/dbgzip.cpp
@@ -0,0 +1,43 @@
+// (c) by Stefan Roettger
+
+#include <mini/minibase.h>
+
+#include <mini/database.h>
+#include <mini/convbase.h>
+
+int main(int argc,char *argv[])
+   {
+   databuf buf;
+   BOOLINT extfmt_z;
+
+   convbase::MINI_CONVERSION_PARAMS conversion_params;
+
+   if (argc!=3)
+      {
+      printf("usage: %s <input.db> <output.db>\n",argv[0]);
+      exit(1);
+      }
+
+   // register libMini conversion hook
+   convbase::setparameters(&conversion_params);
+   convbase::setconversion(&conversion_params);
+
+   // load stub
+   if (buf.loaddata(argv[1],1)!=0)
+      {
+      extfmt_z=buf.extformat==databuf::DATABUF_EXTFMT_Z;
+
+      // load buffer
+      if (buf.loaddata(argv[1])!=0)
+         {
+         // save buffer
+         if (extfmt_z) buf.savedata(argv[2]);
+         else buf.savedata(argv[2],databuf::DATABUF_EXTFMT_Z);
+
+         // release buffer
+         buf.release();
+         }
+      }
+
+   return(0);
+   }
