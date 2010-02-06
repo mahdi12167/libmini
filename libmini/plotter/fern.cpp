a98c2ba90c0307b91e7d14234a71e3060942b4ce
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2010-02-06 12:13:08 +0000

    brush up fern colors

--- libmini/plotter/fern.cpp
+++ libmini/plotter/fern.cpp
@@ -9,47 +9,67 @@ void fern(int n)
    double x,y;
    double xn,yn;
 
+   double r,g,b;
+
    double z;
+
+   static const double z1=0.01;
+   static const double z2=0.07;
+   static const double z3=0.07;
+   static const double z4=0.85;
 
    x=0.0;
    y=0.0;
+
+   r=0.0;
+   g=0.0;
+   b=0.0;
 
    for (i=0; i<n; i++)
       {
       z=drand48();
 
-      if (z<0.01)
+      if ((z-=z1)<0.0)
          {
          xn=0.0;
          yn=0.16*y;
 
-         plot_color(0.75f,0.5f,0.0f);
+         r=r*z1+0.75*(1.0-z1);
+         g=g*z1+0.5*(1.0-z1);
+         b=b*z1+0.1*(1.0-z1);
          }
-      else if (z<0.08)
+      else if ((z-=z2)<0.0)
          {
          xn=0.2*x-0.26*y;
          yn=0.23*x+0.22*y+1.6;
 
-         plot_color(0.25f,1.0f,0.75f);
+         r=r*z2+0.25*(1.0-z2);
+         g=g*z2+1.0*(1.0-z2);
+         b=b*z2+0.75*(1.0-z2);
          }
-      else if (z<0.15)
+      else if ((z-=z3)<0.0)
          {
          xn=-0.15*x+0.28*y;
          yn=0.26*x+0.24*y+0.44;
 
-         plot_color(0.5f,1.0f,0.5f);
+         r=r*z3+0.5*(1.0-z3);
+         g=g*z3+1.0*(1.0-z3);
+         b=b*z3+0.5*(1.0-z3);
          }
       else
          {
          xn=0.85*x+0.04*y;
          yn=-0.04*x+0.85*y+1.6;
 
-         plot_color(0.25f,1.0f,0.25f);
+         r=r*z4+0.1*(1.0-z4);
+         g=g*z4+0.5*(1.0-z4);
+         b=b*z4+0.1*(1.0-z4);
          }
 
       x=xn;
       y=yn;
 
+      plot_color(r,g,b);
       plot_point(0.075*x+0.5,0.075*y+0.1);
       }
    }
