018ba3e994e7d78a918bb025dc1d437b393f3b7c
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2010-03-16 10:41:40 +0000

    for convenience, add installation script for libMini dependencies

---
+++ libmini/deps/install.sh
@@ -0,0 +1,16 @@
+#!/bin/tcsh -f
+
+# this script installs the libMini dependencies locally
+
+set prefix=$cwd/../..
+
+./build.sh
+
+# curl
+(cd curl; ./buildconf; ./configure --prefix=$prefix/libcurl; make; make install)
+
+# squish
+(cd squish; cmake .; make)
+
+# freeglut
+(cd freeglut; ./autogen.sh; ./configure --prefix=$prefix/freeglut; make; make install)
