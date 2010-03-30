430d34b5dad441fdb4b9a6e13f09b23facf5cbed
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2010-03-30 21:39:36 +0000

    improve script for installing dependencies locally

--- libmini/deps/install.sh
+++ libmini/deps/install.sh
@@ -7,10 +7,12 @@ set prefix=$cwd/../..
 ./build.sh
 
 # curl
-(cd curl; ./buildconf; ./configure --prefix=$prefix/libcurl; make; make install)
+(cd curl; ./buildconf; ./configure --prefix=$prefix/libcurl; make -j 2; make install)
 
 # squish
-(cd squish; cmake .; make)
+(cd squish; cmake -DCMAKE_INSTALL_PREFIX=$prefix/squish; make -j 2; make install)
 
 # freeglut
-(cd freeglut; autoreconf --force --install; ./autogen.sh; ./configure --prefix=$prefix/freeglut; make; make install)
+if ($HOSTTYPE != "intel-pc" && $HOSTTYPE != "intel-mac") then # skip freeglut on MacOS X
+   (cd freeglut; autoreconf --force --install; ./autogen.sh; ./configure --prefix=$prefix/freeglut; make -j 2; make install)
+endif
