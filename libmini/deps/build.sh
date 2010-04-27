a3e9cfb0cd325fa7d7fcf0a1bbffcfbb4f85703a
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2010-04-27 19:13:08 +0000

    update pull script to reflect libcurl's move to git

--- libmini/deps/build.sh
+++ libmini/deps/build.sh
@@ -1,23 +1,25 @@
 #!/bin/tcsh -f
 
 # this script pulls the most recent tested versions of the libMini dependencies
-# pthreads, libjpeg and libpng/libz are assumed to be installed on the system by default
-# then the remaining dependencies are:
+# the pthreads, libjpeg and libpng/libz libraries are assumed to be installed
+# then the remaining dependencies to pull are:
 
-# curl
-cvs -d:pserver:anonymous:@cool.haxx.se:/cvsroot/curl login
-cvs -d:pserver:anonymous@cool.haxx.se:/cvsroot/curl co -r curl-7_19_5 curl
+# curl 7.20.1
+if (! -e curl) then
+   git clone git://curl.haxx.se/curl
+endif
+(cd curl; git checkout curl-7_20_1)
 
-# squish
+# squish 1.10
 svn co -r 26 http://libsquish.googlecode.com/svn/trunk libsquish
 foreach file (libsquish/*.cpp libsquish/*.h libsquish/*.inl libsquish/CMakeLists.txt)
    cp -f $file $file:s/libsquish/squish/
    ../mini/tabify.sh -x $file:s/libsquish/squish/
 end
 
-# GREYCstoration
+# GREYCstoration 1.3.4
 cvs -d:pserver:anonymous:@cimg.cvs.sourceforge.net:/cvsroot/cimg login
-cvs -d:pserver:anonymous@cimg.cvs.sourceforge.net:/cvsroot/cimg co -r 1.361 CImg/CImg.h
+cvs -d:pserver:anonymous@cimg.cvs.sourceforge.net:/cvsroot/cimg co -r 1.627 CImg/CImg.h
 foreach file (CImg.h)
    cp -f CImg/$file greycstoration/$file
    ../mini/tabify.sh -x greycstoration/$file
