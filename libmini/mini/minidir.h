60dc695101debea038a033e73ad33012ae0155ef
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2010-06-21 20:15:12 +0000

    minor fix

--- libmini/mini/minidir.h
+++ libmini/mini/minidir.h
@@ -6,7 +6,7 @@
 namespace minidir {
 
 // specify file search path and pattern (with '*' as single wildcard)
-void filesearch(const char *spec=NULL);
+void filesearch(const char *spec);
 
 // find next file matching the search pattern
 const char *findfile();
