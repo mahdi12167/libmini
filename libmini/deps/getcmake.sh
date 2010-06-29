36ecb514669ec59205b4461fa4a6261320eea6e0
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2010-06-29 14:39:31 +0000

    same for cmake

---
+++ libmini/deps/getcmake.sh
@@ -0,0 +1,12 @@
+#!/bin/tcsh -f
+
+# this script pulls and installs cmake
+
+set cmake=cmake-2.8.2
+
+if (! -X cmake) then
+   wget http://www.cmake.org/files/v2.8/$cmake.tar.gz
+   tar zxf $cmake.tar.gz
+
+   (cd $cmake; ./configure --prefix=$HOME; make; make install)
+end
