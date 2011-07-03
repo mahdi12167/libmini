637fa61b4c40c634b63497413bb66ae3cbc0bf39
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2011-07-03 19:17:29 +0000

    tweak

--- libmini/qtviewer/mainwindow.cpp
+++ libmini/qtviewer/mainwindow.cpp
@@ -1,72 +1,63 @@
 #include <QtGui>
+
 #include "mainwindow.h"
 #include "viewerwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+   : QMainWindow(parent)
 {
+   viewerWindow = new ViewerWindow();
+   setCentralWidget(viewerWindow);
 
-       viewerWindow = new ViewerWindow();
-       setCentralWidget(viewerWindow);
-
-       createActions();
-       createMenus();
-       createStatusBar();
+   createActions();
+   createMenus();
+   createStatusBar();
 }
 
-MainWindow::~MainWindow()
-{
-
-}
+MainWindow::~MainWindow() {}
 
 void MainWindow::createActions()
 {
-    openAction = new QAction(tr("&Open"), this);
-    openAction->setIcon(QIcon(":/images/open.png"));
-    openAction->setShortcut(tr("Ctrl+O"));
-    openAction->setStatusTip(tr("Open an existing file"));
-    connect(openAction, SIGNAL(triggered()), this, SLOT(open()));
+   openAction = new QAction(tr("&Open"), this);
+   openAction->setIcon(QIcon(":/images/open.png"));
+   openAction->setShortcut(tr("Ctrl+O"));
+   openAction->setStatusTip(tr("Open an existing file"));
+   connect(openAction, SIGNAL(triggered()), this, SLOT(open()));
 
-    aboutAction = new QAction(tr("&About"), this);
-    aboutAction->setIcon(QIcon(":/images/about.png"));
-    aboutAction->setShortcut(tr("Ctrl+A"));
-    aboutAction->setStatusTip(tr("About information"));
-    connect(openAction, SIGNAL(triggered()), this, SLOT(about()));
+   aboutAction = new QAction(tr("&About"), this);
+   aboutAction->setIcon(QIcon(":/images/about.png"));
+   aboutAction->setShortcut(tr("Ctrl+A"));
+   aboutAction->setStatusTip(tr("About information"));
+   connect(openAction, SIGNAL(triggered()), this, SLOT(about()));
 }
 
 void MainWindow::createMenus()
 {
-    fileMenu = menuBar()->addMenu(tr("&File"));
-    fileMenu->addAction(openAction);
-    //fileMenu->addAction(exitAction);
+   fileMenu = menuBar()->addMenu(tr("&File"));
+   fileMenu->addAction(openAction);
 
-    menuBar()->addSeparator();
+   menuBar()->addSeparator();
 
-    helpMenu = menuBar()->addMenu(tr("&Help"));
-    helpMenu->addAction(aboutAction);
+   helpMenu = menuBar()->addMenu(tr("&Help"));
+   helpMenu->addAction(aboutAction);
 }
 
 void MainWindow::createStatusBar()
 {
-    locationLabel = new QLabel(" 999,999,999 ");
-    locationLabel->setAlignment(Qt::AlignHCenter);
-    locationLabel->setMinimumSize(locationLabel->sizeHint());
+   locationLabel = new QLabel("");
+   locationLabel->setAlignment(Qt::AlignHCenter);
+   locationLabel->setMinimumSize(locationLabel->sizeHint());
 
-    messageLabel = new QLabel();
-    messageLabel->setIndent(3);
+   messageLabel = new QLabel();
+   messageLabel->setIndent(3);
 
-    statusBar()->addWidget(locationLabel);
-    statusBar()->addWidget(messageLabel, 1);
-
-    //updateStatusBar();
+   statusBar()->addWidget(locationLabel);
+   statusBar()->addWidget(messageLabel, 1);
 }
 
 void MainWindow::open()
 {
-    QMessageBox::information(this, tr("Open file"), tr("Open existing file"), QMessageBox::Yes | QMessageBox::Default, QMessageBox::No);
+   QMessageBox::information(this, tr("Open file"), tr("Open existing file"), QMessageBox::Yes | QMessageBox::Default, QMessageBox::No);
 }
 
-void MainWindow::about()
-{
-
-}
+void MainWindow::about() {}
--- libmini/qtviewer/mainwindow.h
+++ libmini/qtviewer/mainwindow.h
@@ -9,34 +9,30 @@ class ViewerWindow;
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+   Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-
-protected:
-    //void closeEvent(QCloseEvent *);
+   MainWindow(QWidget *parent = 0);
+   ~MainWindow();
 
 private slots:
-    void open();
-    void about();
+   void open();
+   void about();
 
 private:
-    void createActions();
-    void createMenus();
-    void createStatusBar();
+   void createActions();
+   void createMenus();
+   void createStatusBar();
 
+   QLabel* locationLabel;
+   QLabel* messageLabel;
+   QMenu* fileMenu;
+   QMenu* helpMenu;
 
-    QLabel* locationLabel;
-    QLabel* messageLabel;
-    QMenu*  fileMenu;
-    QMenu*  helpMenu;
+   QAction* openAction;
+   QAction* aboutAction;
 
-    QAction*    openAction;
-    QAction*    aboutAction;
-
-    ViewerWindow*   viewerWindow;
+   ViewerWindow* viewerWindow;
 };
 
-#endif // MAINWINDOW_H
+#endif
