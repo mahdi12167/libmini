b6faa474db963282c0460ed07b3e332fa79ed166
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2011-08-12 10:28:13 +0000

    fix dbdump display being upside down

--- libmini/mini/tools/dbdump.cpp
+++ libmini/mini/tools/dbdump.cpp
@@ -87,13 +87,13 @@ void displayfunc()
 
    glColor3f(1.0f,1.0f,1.0f);
    glBegin(GL_TRIANGLE_FAN);
-   glTexCoord2f(0.0f,0.0f);
-   glVertex3f(0.0f,0.0f,0.0f);
-   glTexCoord2f(1.0f,0.0f);
-   glVertex3f(1.0f,0.0f,0.0f);
-   glTexCoord2f(1.0f,1.0f);
-   glVertex3f(1.0f,1.0f,0.0f);
    glTexCoord2f(0.0f,1.0f);
+   glVertex3f(0.0f,0.0f,0.0f);
+   glTexCoord2f(1.0f,1.0f);
+   glVertex3f(1.0f,0.0f,0.0f);
+   glTexCoord2f(1.0f,0.0f);
+   glVertex3f(1.0f,1.0f,0.0f);
+   glTexCoord2f(0.0f,0.0f);
    glVertex3f(0.0f,1.0f,0.0f);
    glEnd();
 
