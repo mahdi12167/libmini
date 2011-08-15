3cbecdd5eb319e9e7ce3366a7e233353c6f8c989
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2011-08-15 19:38:46 +0000

    comment build script for earth and night textures

--- libmini/data/build.sh
+++ libmini/data/build.sh
@@ -1,20 +1,29 @@
 #!/bin/tcsh -f
 
+# the script requires two images as arguments
+# the images are assumed to be day and night earth textures
+# both need to be in LatLon coordinate layout
+# such as the BlueMarble collection of images
 if ($1 == "" || $2 == "") exit
 
 set day=$1
 set ngt=$2
 
+# convert day image to required resolutions
 convert $day -geometry 512x256! EarthDay.png
+convert $day -geometry 1024x512! EarthDay-1K.png
 convert $day -geometry 2048x1024! EarthDay-2K.png
 convert $day -geometry 4096x2048! EarthDay-4K.png
 convert $day -geometry 8192x4096! EarthDay-8K.png
 
+# convert night image to required resolutions
 convert $ngt -geometry 512x256! EarthNight.png
+convert $ngt -geometry 1024x512! EarthNight-1K.png
 convert $ngt -geometry 2048x1024! EarthNight-2K.png
 convert $ngt -geometry 4096x2048! EarthNight-4K.png
 convert $ngt -geometry 8192x4096! EarthNight-8K.png
 
+# convert into db format /w s3tc and mipmap
 foreach image (Earth*.png)
    ../mini/tools/db2db $image $image:r.db
 end
