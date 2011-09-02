c28d09cd9e4dfb960b5b69d52526224fe51f7d40
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2011-09-02 21:42:14 +0000

    be more verbose about missing qmake

--- libmini/qtviewer/build.sh
+++ libmini/qtviewer/build.sh
@@ -3,6 +3,11 @@
 # unix build script
 # (c) by Stefan Roettger
 
+if (! -X qmake) then
+   echo qmake not installed!
+   exit 1
+endif
+
 set options=""
 if ($HOSTTYPE == "intel-pc") set options="-spec macx-g++"
 
