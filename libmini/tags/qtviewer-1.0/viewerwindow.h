2ca92b344c52faa6ee4393f9e4d7c631dfa720ea
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2011-09-07 10:47:39 +0000

    tag qtviewer v1.0

---
+++ libmini/tags/qtviewer-1.0/viewerwindow.h
@@ -0,0 +1,65 @@
+#ifndef VIEWERWINDOW_H
+#define VIEWERWINDOW_H
+
+#include <QtOpenGL/qgl.h>
+
+#include <QtCore/QTime>
+#include <QtCore/QPoint>
+
+#include <QtGui/QMouseEvent>
+
+class Renderer;
+
+class QMimeData;
+
+class ViewerWindow : public QGLWidget
+{
+   Q_OBJECT;
+
+public:
+   ViewerWindow(QWidget* parent = 0);
+   virtual ~ViewerWindow();
+
+   QSize minimumSizeHint() const;
+   QSize sizeHint() const;
+
+   void loadMap(QString url);
+   void clearMaps();
+
+   void gotoMap(int n);
+
+signals:
+   void changed(const QString url);
+
+protected:
+   void initializeGL();
+   void resizeGL(int width, int height);
+   void paintGL();
+
+   void mousePressEvent(QMouseEvent *event);
+   void mouseReleaseEvent(QMouseEvent* event);
+   void mouseMoveEvent(QMouseEvent *event);
+   void mouseDoubleClickEvent(QMouseEvent *);
+
+   void keyPressEvent(QKeyEvent* event);
+   void keyReleaseEvent(QKeyEvent* event);
+
+   void wheelEvent(QWheelEvent *event);
+
+   void timerEvent(QTimerEvent *);
+
+   void dragEnterEvent(QDragEnterEvent *event);
+   void dragMoveEvent(QDragMoveEvent *event);
+   void dropEvent(QDropEvent *event);
+   void dragLeaveEvent(QDragLeaveEvent *event);
+
+private:
+   Renderer* renderer;
+
+   QPoint    lastPos;
+   QPoint    movedPos;
+   bool      bLeftButtonDown;
+   bool      bRightButtonDown;
+};
+
+#endif
