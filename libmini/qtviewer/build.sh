36a1db3afde64715fc55630760fc3129f806f845
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2011-09-08 16:48:03 +0000

    add zip rule to build script

--- libmini/qtviewer/build.sh
+++ libmini/qtviewer/build.sh
@@ -3,6 +3,11 @@
 # unix build script
 # (c) by Stefan Roettger
 
+if ($1 == "zip") then
+   (cd ..; zip -qr9 qtviewer/QTVIEWER.zip qtviewer -x \*/.\?\?\* -x \*CVS/\*)
+   exit
+endif
+
 if (! -X qmake) then
    echo qmake not installed!
    exit 1
