ee1855ae0ca1c550ad021aad2babedccc901b4ad
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2011-09-15 21:29:01 +0000

    draw pole stubs

--- libmini/qtviewer/renderer.cpp
+++ libmini/qtviewer/renderer.cpp
@@ -921,25 +921,11 @@ void Viewer::render_ecef_geometry()
    static const miniv3d yacolor(0.0,1.0,0.0);
    static const miniv3d zacolor(0.0,0.0,1.0);
 
-#if 0
-   color(0.5*xacolor);
-   renderline(miniv3d(-1.1*miniearth::EARTH_radius,0.0,0.0),
-              miniv3d(0.0,0.0,0.0));
-   color(xacolor);
-   renderline(miniv3d(0.0,0.0,0.0),
-              miniv3d(1.1*miniearth::EARTH_radius,0.0,0.0));
-   color(0.5*yacolor);
-   renderline(miniv3d(0.0,-1.1*miniearth::EARTH_radius,0.0),
-              miniv3d(0.0,0.0,0.0));
-   color(yacolor);
-   renderline(miniv3d(0.0,0.0,0.0),
-              miniv3d(0.0,1.1*miniearth::EARTH_radius,0.0));
-#endif
    color(0.5*zacolor);
    renderline(miniv3d(0.0,0.0,-1.1*miniearth::EARTH_radius),
-              miniv3d(0.0,0.0,0.0));
+              miniv3d(0.0,0.0,-miniearth::EARTH_radius));
    color(zacolor);
-   renderline(miniv3d(0.0,0.0,0.0),
+   renderline(miniv3d(0.0,0.0,miniearth::EARTH_radius),
               miniv3d(0.0,0.0,1.1*miniearth::EARTH_radius));
 
    // render equator:
