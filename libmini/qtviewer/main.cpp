0a33dbac10e4cd8c9e7d5ad8fbf2b2f220e8a038
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2011-10-17 12:36:48 +0000

    add error handler

--- libmini/qtviewer/main.cpp
+++ libmini/qtviewer/main.cpp
@@ -1,11 +1,22 @@
 // (c) by Stefan Roettger
 
+#include <mini/mini.h>
+
 #include <QtGui/QApplication>
 #include <QtOpenGL/qgl.h>
 
 #include <QtGui/QMessageBox>
 
 #include "mainwindow.h"
+
+void errormsg(const char *file,int line,int fatal)
+{
+   if (fatal==MINI_ERROR_NONFATAL) fprintf(stderr,"warning");
+   else if (fatal==MINI_ERROR_MEM) fprintf(stderr,"insufficient memory");
+   else if (fatal==MINI_ERROR_IO) fprintf(stderr,"io error");
+   else fprintf(stderr,"fatal error");
+   fprintf(stderr," in <%s> at line %d!\n",file,line);
+}
 
 int main(int argc, char *argv[])
 {
@@ -19,6 +30,8 @@ int main(int argc, char *argv[])
 
       return(1);
    }
+
+   setminierrorhandler(errormsg);
 
    MainWindow window;
 
