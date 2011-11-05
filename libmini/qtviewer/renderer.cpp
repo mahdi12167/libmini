b19f0910e3fc565dd66454f1f47e1285664d8a42
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2011-11-05 10:37:42 +0000

    add extension example to README

--- libmini/qtviewer/renderer.cpp
+++ libmini/qtviewer/renderer.cpp
@@ -19,11 +19,11 @@ mininode *Renderer::build_ecef_geometry()
    mininoderef pole=mininoderef(new mininode_geometry_tube(10000,500000));
 
    axis->append_child(new mininode_coord(minicoord(miniv3d(0,-90*3600,0),minicoord::MINICOORD_LLH)))->
-      append_child(new mininode_color(miniv3d(0,0,0.5)))->
+      append_child(new mininode_color(miniv3d(0.0,0.0,0.5)))->
       append_child(pole);
 
    axis->append_child(new mininode_coord(minicoord(miniv3d(0,90*3600,0),minicoord::MINICOORD_LLH)))->
-      append_child(new mininode_color(miniv3d(0,0,1.0)))->
+      append_child(new mininode_color(miniv3d(0.0,0.0,1.0)))->
       append_child(pole);
 
    group->append_child(axis);
