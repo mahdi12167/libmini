19191e4a6f038b62cfce0180655e603e040eed54
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2011-11-17 07:35:22 +0000

    add test geometry

--- libmini/qtviewer/renderer.cpp
+++ libmini/qtviewer/renderer.cpp
@@ -43,5 +43,41 @@ mininode *Renderer::build_ecef_geometry()
    group->append_child(new mininode_color(miniv3d(0.5,0.5,0.5)))->
       append_child(new mininode_geometry_band(pos,pos,20000));
 
+#ifdef TEST
+
+   // tetrahedron:
+
+   group->append_child(new mininode_coord(minicoord(miniv3d(-159*3600,21*3600,0),minicoord::MINICOORD_LLH)))->
+      append_child(new mininode_color(miniv3d(0.5,0.5,0.5)))->
+      append_child(new mininode_scale(10000))->
+      append_child(new mininode_translate(miniv3d(0,0,5)))->
+      append_child(new mininode_geometry_tet(10));
+
+   // house:
+
+   // define local coordinate system at Lat/Lon=49/11
+   mininode *house=new mininode_coord(minicoord(miniv3d(11*3600,49*3600,0),minicoord::MINICOORD_LLH));
+
+   // scale up by a factor of 10000
+   mininode *scale=house->append_child(new mininode_scale(10000));
+
+   // white cube with base size 10/10 and height 5 meters
+   // origin of the cube is its barycenter
+   // translate up 2.5 meters
+   scale->append_child(new mininode_color(miniv3d(1.0,1.0,1.0)))->
+      append_child(new mininode_translate(miniv3d(0,0,2.5)))->
+      append_child(new mininode_geometry_cube(10,10,5));
+
+   // grey prism as roof with base size 10,10
+   // origin of the prism is the center of its base plane
+   // translate up 5 meters
+   scale->append_child(new mininode_color(miniv3d(0.5,0.5,0.5)))->
+      append_child(new mininode_translate(miniv3d(0,0,5)))->
+      append_child(new mininode_geometry_prism(10,10,2.5));
+
+   group->append_child(house);
+
+#endif
+
    return(group);
 }
