92abe69780514af32188a2f338c1d8e04adfd11f
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2011-12-29 14:09:21 +0000

    add perlin noise module

---
+++ libmini/mini/mininoise.cpp
@@ -0,0 +1,228 @@
+// (c) by Stefan Roettger
+
+#include "minibase.h"
+
+#include "mininoise.h"
+
+float noise_interpolate(float v0,float v1,float v2,float v3,float x)
+   {
+   float p,q,r;
+
+   p=v3-v2+v1-v0;
+   q=v0-v1-p;
+   r=v2-v0;
+
+   return(((p*x+q)*x+r)*x+v1);
+   }
+
+float noise_interpolate(float *octave,int sx,int sy,int sz,
+                        float c1,float c2,float c3)
+   {
+   int k1,k2,k3;
+   float w1,w2,w3;
+
+   float v0,v1,v2,v3,
+         v4,v5,v6,v7,
+         v8,v9,v10,v11,
+         v12,v13,v14,v15;
+
+   k1=ftrc(c1*(sx-1));
+   w1=c1*(sx-1)-k1;
+
+   if (k1<0)
+      {
+      k1=0;
+      w1=0.0f;
+      }
+
+   if (k1>sx-2)
+      {
+      k1=sx-2;
+      w1=1.0f;
+      }
+
+   k2=ftrc(c2*(sy-1));
+   w2=c2*(sy-1)-k2;
+
+   if (k2<0)
+      {
+      k2=0;
+      w2=0.0f;
+      }
+
+   if (k2>sy-2)
+      {
+      k2=sy-2;
+      w2=1.0f;
+      }
+
+   k3=ftrc(c3*(sz-1));
+   w3=c3*(sz-1)-k3;
+
+   if (k3<0)
+      {
+      k3=0;
+      w3=0.0f;
+      }
+
+   if (k3>sz-2)
+      {
+      k3=sz-2;
+      w3=1.0f;
+      }
+
+   v1=noise_interpolate((k1>0)?octave[k1-1+(k2+k3*sy)*sx]:octave[k1+(k2+k3*sy)*sx],
+                        octave[k1+(k2+k3*sy)*sx],octave[k1+1+(k2+k3*sy)*sx],
+                        (k1<sx-2)?octave[k1+2+(k2+k3*sy)*sx]:octave[k1+1+(k2+k3*sy)*sx],w1);
+
+   if (k2>0)
+      v0=noise_interpolate((k1>0)?octave[k1-1+(k2-1+k3*sy)*sx]:octave[k1+(k2-1+k3*sy)*sx],
+                           octave[k1+(k2-1+k3*sy)*sx],octave[k1+1+(k2-1+k3*sy)*sx],
+                           (k1<sx-2)?octave[k1+2+(k2-1+k3*sy)*sx]:octave[k1+1+(k2-1+k3*sy)*sx],w1);
+   else v0=v1;
+
+   v2=noise_interpolate((k1>0)?octave[k1-1+(k2+1+k3*sy)*sx]:octave[k1+(k2+1+k3*sy)*sx],
+                        octave[k1+(k2+1+k3*sy)*sx],octave[k1+1+(k2+1+k3*sy)*sx],
+                        (k1<sx-2)?octave[k1+2+(k2+1+k3*sy)*sx]:octave[k1+1+(k2+1+k3*sy)*sx],w1);
+
+   if (k2<sy-2)
+      v3=noise_interpolate((k1>0)?octave[k1-1+(k2+2+k3*sy)*sx]:octave[k1+(k2+2+k3*sy)*sx],
+                           octave[k1+(k2+2+k3*sy)*sx],octave[k1+1+(k2+2+k3*sy)*sx],
+                           (k1<sx-2)?octave[k1+2+(k2+2+k3*sy)*sx]:octave[k1+1+(k2+2+k3*sy)*sx],w1);
+   else v3=v2;
+
+   v5=noise_interpolate((k1>0)?octave[k1-1+(k2+(k3+1)*sy)*sx]:octave[k1+(k2+(k3+1)*sy)*sx],
+                        octave[k1+(k2+(k3+1)*sy)*sx],octave[k1+1+(k2+(k3+1)*sy)*sx],
+                        (k1<sx-2)?octave[k1+2+(k2+(k3+1)*sy)*sx]:octave[k1+1+(k2+(k3+1)*sy)*sx],w1);
+
+   if (k2>0)
+      v4=noise_interpolate((k1>0)?octave[k1-1+(k2-1+(k3+1)*sy)*sx]:octave[k1+(k2-1+(k3+1)*sy)*sx],
+                           octave[k1+(k2-1+(k3+1)*sy)*sx],octave[k1+1+(k2-1+(k3+1)*sy)*sx],
+                           (k1<sx-2)?octave[k1+2+(k2-1+(k3+1)*sy)*sx]:octave[k1+1+(k2-1+(k3+1)*sy)*sx],w1);
+   else v4=v5;
+
+   v6=noise_interpolate((k1>0)?octave[k1-1+(k2+1+(k3+1)*sy)*sx]:octave[k1+(k2+1+(k3+1)*sy)*sx],
+                        octave[k1+(k2+1+(k3+1)*sy)*sx],octave[k1+1+(k2+1+(k3+1)*sy)*sx],
+                        (k1<sx-2)?octave[k1+2+(k2+1+(k3+1)*sy)*sx]:octave[k1+1+(k2+1+(k3+1)*sy)*sx],w1);
+
+   if (k2<sy-2)
+      v7=noise_interpolate((k1>0)?octave[k1-1+(k2+2+(k3+1)*sy)*sx]:octave[k1+(k2+2+(k3+1)*sy)*sx],
+                           octave[k1+(k2+2+(k3+1)*sy)*sx],octave[k1+1+(k2+2+(k3+1)*sy)*sx],
+                           (k1<sx-2)?octave[k1+2+(k2+2+(k3+1)*sy)*sx]:octave[k1+1+(k2+2+(k3+1)*sy)*sx],w1);
+   else v7=v6;
+
+   if (k3>0)
+      {
+      v9=noise_interpolate((k1>0)?octave[k1-1+(k2+(k3-1)*sy)*sx]:octave[k1+(k2+(k3-1)*sy)*sx],
+                           octave[k1+(k2+(k3-1)*sy)*sx],octave[k1+1+(k2+(k3-1)*sy)*sx],
+                           (k1<sx-2)?octave[k1+2+(k2+(k3-1)*sy)*sx]:octave[k1+1+(k2+(k3-1)*sy)*sx],w1);
+
+      if (k2>0)
+         v8=noise_interpolate((k1>0)?octave[k1-1+(k2-1+(k3-1)*sy)*sx]:octave[k1+(k2-1+(k3-1)*sy)*sx],
+                              octave[k1+(k2-1+(k3-1)*sy)*sx],octave[k1+1+(k2-1+(k3-1)*sy)*sx],
+                              (k1<sx-2)?octave[k1+2+(k2-1+(k3-1)*sy)*sx]:octave[k1+1+(k2-1+(k3-1)*sy)*sx],w1);
+      else v8=v9;
+
+      v10=noise_interpolate((k1>0)?octave[k1-1+(k2+1+(k3-1)*sy)*sx]:octave[k1+(k2+1+(k3-1)*sy)*sx],
+                            octave[k1+(k2+1+(k3-1)*sy)*sx],octave[k1+1+(k2+1+(k3-1)*sy)*sx],
+                            (k1<sx-2)?octave[k1+2+(k2+1+(k3-1)*sy)*sx]:octave[k1+1+(k2+1+(k3-1)*sy)*sx],w1);
+
+      if (k2<sy-2)
+         v11=noise_interpolate((k1>0)?octave[k1-1+(k2+2+(k3-1)*sy)*sx]:octave[k1+(k2+2+(k3-1)*sy)*sx],
+                               octave[k1+(k2+2+(k3-1)*sy)*sx],octave[k1+1+(k2+2+(k3-1)*sy)*sx],
+                               (k1<sx-2)?octave[k1+2+(k2+2+(k3-1)*sy)*sx]:octave[k1+1+(k2+2+(k3-1)*sy)*sx],w1);
+      else v11=v10;
+      }
+   else
+      {
+      v8=v0;
+      v9=v1;
+      v10=v2;
+      v11=v3;
+      }
+
+   if (k3<sz-2)
+      {
+      v13=noise_interpolate((k1>0)?octave[k1-1+(k2+(k3+2)*sy)*sx]:octave[k1+(k2+(k3+2)*sy)*sx],
+                            octave[k1+(k2+(k3+2)*sy)*sx],octave[k1+1+(k2+(k3+2)*sy)*sx],
+                            (k1<sx-2)?octave[k1+2+(k2+(k3+2)*sy)*sx]:octave[k1+1+(k2+(k3+2)*sy)*sx],w1);
+
+      if (k2>0)
+         v12=noise_interpolate((k1>0)?octave[k1-1+(k2-1+(k3+2)*sy)*sx]:octave[k1+(k2-1+(k3+2)*sy)*sx],
+                               octave[k1+(k2-1+(k3+2)*sy)*sx],octave[k1+1+(k2-1+(k3+2)*sy)*sx],
+                               (k1<sx-2)?octave[k1+2+(k2-1+(k3+2)*sy)*sx]:octave[k1+1+(k2-1+(k3+2)*sy)*sx],w1);
+      else v12=v13;
+
+      v14=noise_interpolate((k1>0)?octave[k1-1+(k2+1+(k3+2)*sy)*sx]:octave[k1+(k2+1+(k3+2)*sy)*sx],
+                            octave[k1+(k2+1+(k3+2)*sy)*sx],octave[k1+1+(k2+1+(k3+2)*sy)*sx],
+                            (k1<sx-2)?octave[k1+2+(k2+1+(k3+2)*sy)*sx]:octave[k1+1+(k2+1+(k3+2)*sy)*sx],w1);
+
+      if (k2<sy-2)
+         v15=noise_interpolate((k1>0)?octave[k1-1+(k2+2+(k3+2)*sy)*sx]:octave[k1+(k2+2+(k3+2)*sy)*sx],
+                               octave[k1+(k2+2+(k3+2)*sy)*sx],octave[k1+1+(k2+2+(k3+2)*sy)*sx],
+                               (k1<sx-2)?octave[k1+2+(k2+2+(k3+2)*sy)*sx]:octave[k1+1+(k2+2+(k3+2)*sy)*sx],w1);
+      else v15=v14;
+      }
+   else
+      {
+      v12=v4;
+      v13=v5;
+      v14=v6;
+      v15=v7;
+      }
+
+   return(noise_interpolate(noise_interpolate(v8,v9,v10,v11,w2),
+                            noise_interpolate(v0,v1,v2,v3,w2),
+                            noise_interpolate(v4,v5,v6,v7,w2),
+                            noise_interpolate(v12,v13,v14,v15,w2),w3));
+   }
+
+float *noise(int sx,int sy,int sz,
+             float persist)
+   {
+   int i,j,k,l;
+
+   int size;
+   int dx,dy,dz;
+
+   float scaling;
+
+   float *noise,*octave;
+
+   if ((sx&(sx-1))!=0 || sx<2) ERRORMSG();
+   if ((sy&(sy-1))!=0 || sy<2) ERRORMSG();
+   if ((sz&(sz-1))!=0 || sz<2) ERRORMSG();
+
+   if ((noise=(float *)malloc(sx*sy*sz*sizeof(float)))==NULL) ERRORMSG();
+   for (i=0; i<sx*sy*sz; i++) noise[i]=0.0f;
+
+   if ((octave=(float *)malloc(sx*sy*sz*sizeof(float)))==NULL) ERRORMSG();
+
+   size=max(sx,max(sy,sz));
+
+   scaling=1.0f;
+
+   for (i=4; i<=size; i*=2)
+      {
+      dx=max(i*sx/size,1);
+      dy=max(i*sy/size,1);
+      dz=max(i*sz/size,1);
+
+      for (j=0; j<dx*dy*dz; j++)
+         octave[j]=drand48()*scaling;
+
+      for (j=0; j<sx; j++)
+         for (k=0; k<sy; k++)
+            for (l=0; l<sz; l++)
+               noise[j+(k+l*sy)*sx]+=noise_interpolate(octave,dx,dy,dz,
+                                                       (float)j/(sx-1),
+                                                       (float)k/(sy-1),
+                                                       (float)l/(sz-1));
+
+      scaling*=persist;
+      }
+
+   free(octave);
+
+   return(noise);
+   }
---
+++ libmini/mini/mininoise.h
@@ -0,0 +1,9 @@
+// (c) by Stefan Roettger
+
+#ifndef MININOISE_H
+#define MININOISE_H
+
+float *noise(int sx=64,int sy=64,int sz=64,
+             float persist=0.5f);
+
+#endif
