9caaec6167bee8ea2c22871c98bf63be3ea451ba
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-01-04 19:46:24 +0000

    fix wood texture rotation and scaling

--- libmini/qtviewer/renderer.cpp
+++ libmini/qtviewer/renderer.cpp
@@ -67,10 +67,11 @@ mininode_group *Renderer::build_ecef_geometry()
    // origin of the cube is its barycenter
    // translate up 2.5 meters
    scale->append_child(new mininode_color(miniv3d(1.0, 1.0, 1.0)))->
-      append_child(new mininode_texgen_rotate(80,miniv3d(1,1,1)))->
-      append_child(new mininode_texgen_translate(miniv3d(0.0,0.0,0.5)))->
+      append_child(new mininode_texgen_translate(miniv3d(0.5,0.5,0.5)))->
+      append_child(new mininode_texgen_rotate(2,miniv3d(0,1,0)))->
+      append_child(new mininode_texgen_rotate(90,miniv3d(1,0,0)))->
       append_child(new mininode_texgen_scale(0.7))->
-      append_child(new mininode_texgen_scale(0.1,0.1,0.2))->
+      append_child(new mininode_texgen_scale(0.1))->
       append_child(new mininode_volume("/Users/roettger/Projects/libmini/qtviewer/data/textures/Wood.pvm"))->
       append_child(new mininode_translate(miniv3d(0.0, 0.0, 2.5)))->
       append_child(new mininode_geometry_cube(10.0, 10.0, 5.0));
