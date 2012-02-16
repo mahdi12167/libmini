7e081eee08916f1e55f7297ac1d3b613a7210212
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-02-16 17:52:00 +0000

    remove outdated method signature

--- libmini/mini/minicurve.h
+++ libmini/mini/minicurve.h
@@ -13,10 +13,9 @@ class minicurve : public minidyna<minicoord>
    minicurve() {}
 
    void append_sector(const minicoord &p1,const minicoord &p2,
-                      int n=0,double maxl=0.0);
+                      int n=0);
 
    void sort();
-   void sample(double maxl=0.0);
 
    private:
 
