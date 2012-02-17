539782e9ba250e1ca1de0cf50ab82a0d9f867f5a
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-02-17 14:24:37 +0000

    update to latest minicurve implementation

--- libmini/qtviewer/mainconst.h
+++ libmini/qtviewer/mainconst.h
@@ -2,6 +2,6 @@
 #define MAINCONST_H
 
 #define VIEWER_NAME "libMini QTviewer"
-#define VIEWER_VERSION "v1.4.1 as of 13.February.2012"
+#define VIEWER_VERSION "v1.4.1 as of 17.February.2012"
 
 #endif
