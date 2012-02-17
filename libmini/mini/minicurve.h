ec9ea2168939f991839c7e6f391c3337fcf546db
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-02-17 09:13:26 +0000

    add getters

--- libmini/mini/minicurve.h
+++ libmini/mini/minicurve.h
@@ -15,7 +15,16 @@ class minicurve : public minidyna<minicoord>
    void append_sector(const minicoord &p1,const minicoord &p2,
                       unsigned int n=0);
 
+   void validate();
    void sort();
+
+   double get_time_start();
+   double get_time_start();
+   double get_time_period();
+
+   double get_time_step_min();
+   double get_time_step_max();
+   double get_time_step_avg();
 
    private:
 
