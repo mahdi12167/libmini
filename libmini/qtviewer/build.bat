28d72afd03ab6547b8358993de3d8de170cfd4a6
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-02-26 23:38:18 +0000

    fix batch

--- libmini/qtviewer/build.bat
+++ libmini/qtviewer/build.bat
@@ -1,11 +1,13 @@
-# if the batch file is not run from the msvc command prompt
-# try to find the vcvars batch file to set the msvc environment
-!set vcbat="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
-!if exist %vcbat% %vcbat%
+@echo off
+
+rem if the batch file is not run from the msvc command prompt
+rem try to find the vcvars batch file to set the msvc environment
+set vcbat="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+if exist %vcbat% %vcbat%
 
 qmake
-!qmake -tp vc
+qmake -tp vc
 nmake release
 
-!copy release\qtviewer.exe .
-!copy ..\..\gdal\bin\gdal*.dll .
+copy release\qtviewer.exe .
+copy ..\..\gdal\bin\gdal*.dll .
