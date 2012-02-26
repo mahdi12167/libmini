d1a776b2099f517504cb05538cb427b0c6b93316
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-02-26 22:02:09 +0000

    try to find vcvars32.bat

--- libmini/qtviewer/build.bat
+++ libmini/qtviewer/build.bat
@@ -1,3 +1,8 @@
+# if the batch file is not run from the msvc command prompt
+# try to find the vcvars batch file to set the msvc environment
+set vcbat="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+if exist %vcbat% %vcbat%
+
 qmake
 qmake -tp vc
 nmake release
