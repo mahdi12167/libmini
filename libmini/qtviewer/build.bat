8684f1f41ba7681fe88ca76a087c8ba6a75d5d54
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-02-27 01:59:17 +0000

    forgot to call batch file

--- libmini/qtviewer/build.bat
+++ libmini/qtviewer/build.bat
@@ -1,7 +1,7 @@
 @rem if the batch file is not run from the msvc command prompt
 @rem try to find the vcvars batch file to set the msvc environment
 @set vcbat="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
-@if exist %vcbat% %vcbat%
+@if exist %vcbat% call %vcbat%
 
 qmake
 @qmake -tp vc
