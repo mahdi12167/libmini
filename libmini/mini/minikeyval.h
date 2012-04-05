2fecb613b65547c8e4737065ba0d4a72cf666158
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-04-05 13:54:06 +0000

    get list of tagged items

--- libmini/mini/minikeyval.h
+++ libmini/mini/minikeyval.h
@@ -72,6 +72,19 @@ class minikeyval
    minidyna<ministring> &get_tags(const ministring &key)
       {return(get_pair(key)->tags);}
 
+   //! get list of tagged items
+   minidyna<Item *> get_tagged_items(const ministring &tag)
+      {
+      minidyna<Item *> list;
+
+      for (unsigned int i=0; i<pairs.getsize(); i++)
+         for (unsigned int j=0; j<pairs[i].tags.getsize(); j++)
+            if (pairs[i].tags[j]==tag)
+               list.append(&pairs[i].val);
+
+      return(list);
+      }
+
    protected:
 
    //! get pair reference from key
