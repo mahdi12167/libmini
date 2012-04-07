c739fc5cc85b0590874894c4718f7026282629af
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-04-07 10:17:22 +0000

    work on object list

--- libmini/qtviewer/objects.cpp
+++ libmini/qtviewer/objects.cpp
@@ -1,7 +1,42 @@
 // (c) by Stefan Roettger, licensed under LGPL 2.1
 
+#include <mini/miniio.h>
+
 #include "objects.h"
+
+object::object(const ministring &name,const minicoord &crd)
+   {
+   filename=name;
+   coord=crd;
+
+   data=NULL;
+   bytes=0;
+
+   node=NULL;
+   }
+
+object::~object()
+   {if (data!=NULL) free(data);}
+
+void object::load()
+   {
+   data=readfile((repo+filename).c_str(),&bytes);
+   if (data==NULL) ERRORMSG();
+   }
 
 objects::objects()
    : minikeyval<object>()
    {}
+
+objects::~objects()
+   {}
+
+void objects::add(const ministring &key,const ministring &filename)
+   {
+   //!!
+   }
+
+object_list list(const ministring &tag)
+   {
+   //!!
+   }
--- libmini/qtviewer/objects.h
+++ libmini/qtviewer/objects.h
@@ -3,27 +3,40 @@
 #ifndef OBJECTS_H
 #define OBJECTS_H
 
+#include <mini/minibase.h>
 #include <mini/ministring.h>
 #include <mini/minikeyval.h>
+#include <mini/minicoord.h>
+#include <mini/mininode.h>
 
 //! object
 class object
    {
    public:
 
-   object()
-      {
-      filename="";
-      data=NULL;
-      }
+   object(const ministring &name="",const minicoord &crd=minicoord());
 
-   ~object() {}
+   ~object();
 
-   ministring filename;
-   void *data;
+   ministring repo; // data repository
+   ministring filename; // data file in repository
+
+   minicoord coord; // barycenter
+
+   protected:
+
+   void *data; // data chunk
+   unsigned int bytes; // data chunk length
+
+   mininode *node; // node reference
+
+   void load();
    };
 
 //! object list
+typedef minidyna<object *> object_list;
+
+//! object container
 class objects: public minikeyval<object>
    {
    public:
@@ -32,7 +45,10 @@ class objects: public minikeyval<object>
    objects();
 
    //! default destructor
-   ~objects() {}
+   ~objects();
+
+   void add(const ministring &key,const ministring &filename);
+   object_list list(const ministring &tag);
    };
 
 #endif
