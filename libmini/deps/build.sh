7423a1e42697c9cedbe3deaa1e5349e67dc7c8b4
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-04-20 14:02:32 +0000

    add home bin directory to path

--- libmini/deps/build.sh
+++ libmini/deps/build.sh
@@ -3,6 +3,11 @@
 # this script pulls and installs the libMini dependencies
 # the installation path can be supplied as an optional argument (default is ../..)
 
+set homebin=~/bin
+setenv PATH "$PATH"":""$homebin"
+echo $PATH
+rehash
+
 foreach app (wget git cmake)
    if (! -X $app) then
       ./get"$app".sh
