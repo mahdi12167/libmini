a84744041509181e9daca981e5b144c162bae756
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-05-15 11:04:10 +0000

    additional sorting methods for templated dynamic arrays

--- libmini/mini/minisort.h
+++ libmini/mini/minisort.h
@@ -138,6 +138,72 @@ void shellsort(minidyna<Item *> &a)
       }
    }
 
+// templated version for dynamic arrays with less comparison function
+template <class Item>
+void shellsort(minidyna<Item> &a,BOOLINT (*less)(const Item &a,const Item &b))
+   {
+   unsigned int i,j,h;
+
+   unsigned int n;
+
+   Item v;
+
+   n=a.getsize();
+
+   if (n==0) return;
+
+   for (h=1; h<=(n-1)/9; h=3*h+1);
+
+   while (h>0)
+      {
+      for (i=h; i<n; i++)
+         {
+         j=i;
+         v=a[i];
+         while (j>=h && less(v,a[j-h]))
+            {
+            a[j]=a[j-h];
+            j-=h;
+            }
+         if (j!=i) a[j]=v;
+         }
+      h/=3;
+      }
+   }
+
+// templated version for dynamic pointer arrays with less comparison function
+template <class Item>
+void shellsort(minidyna<Item *> &a,BOOLINT (*less)(const Item &a,const Item &b))
+   {
+   unsigned int i,j,h;
+
+   unsigned int n;
+
+   Item *v;
+
+   n=a.getsize();
+
+   if (n==0) return;
+
+   for (h=1; h<=(n-1)/9; h=3*h+1);
+
+   while (h>0)
+      {
+      for (i=h; i<n; i++)
+         {
+         j=i;
+         v=a[i];
+         while (j>=h && less((*v),(*a[j-h])))
+            {
+            a[j]=a[j-h];
+            j-=h;
+            }
+         if (j!=i) a[j]=v;
+         }
+      h/=3;
+      }
+   }
+
 }
 
 using namespace minisort;
