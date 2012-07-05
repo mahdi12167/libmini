a8e30df8e70605472b741ad16755e934a0b553b1
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-07-05 23:18:06 +0000

    add windows wrappers for remove and rmdir

--- libmini/mini/minidir.h
+++ libmini/mini/minidir.h
@@ -3,6 +3,12 @@
 #ifndef MINIDIR_H
 #define MINIDIR_H
 
+#ifdef _WIN32
+#include <windows.h>
+inline void remove(const char *file) {DeleteFile(file);}
+inline void rmdir(const char *dir) {RemoveDirectory(dir);}
+#endif
+
 namespace minidir {
 
 // specify file search path and pattern (with '*' as single wildcard)
