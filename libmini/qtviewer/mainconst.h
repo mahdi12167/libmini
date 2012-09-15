3fcf8ef2f8906a18046bc36ca098ba862a54df8a
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-09-15 09:14:50 +0000

    move to v1.6

--- libmini/qtviewer/mainconst.h
+++ libmini/qtviewer/mainconst.h
@@ -3,7 +3,7 @@
 #ifndef MAINCONST_H
 #define MAINCONST_H
 
-#define VIEWER_NAME "libMini QTviewer"
-#define VIEWER_VERSION "v1.5.2 as of 13.September.2012"
+#define VIEWER_NAME "QTviewer"
+#define VIEWER_VERSION "v1.6 as of 15.September.2012"
 
 #endif
