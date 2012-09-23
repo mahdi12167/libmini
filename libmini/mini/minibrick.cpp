ad2f04ac2c51372880accc5dcf5bf4f9f91165cf
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2012-09-23 13:56:27 +0000

    treat alpha channel of minibrick textures as mask, do not interpolate mask values

--- libmini/mini/minibrick.cpp
+++ libmini/mini/minibrick.cpp
@@ -665,6 +665,7 @@ void minisurf::enabletorch(int phase,
       MOV pos,fragment.texcoord[2]; \n\
       ### fetch actual texel \n\
       TEX tex,fragment.texcoord[0],texture[0],2D; \n\
+      SLT tex.w,1,tex.w; \n\
       ### modulate with texture color \n\
       MUL col,col,tex; \n\
       ### apply head light to color and opacity \n\
@@ -697,6 +698,7 @@ void minisurf::enabletorch(int phase,
       MOV pos,fragment.texcoord[2]; \n\
       ### fetch actual texel \n\
       TEX tex,fragment.texcoord[0],texture[0],3D; \n\
+      SLT tex.w,1,tex.w; \n\
       ### modulate with texture color \n\
       MUL col,col,tex; \n\
       ### apply head light to color and opacity \n\
@@ -950,6 +952,7 @@ void minisurf::enablepattern(float ambient,
       MOV crd,fragment.texcoord[3]; \n\
       ### fetch actual texel \n\
       TEX tex,fragment.texcoord[0],texture[0],2D; \n\
+      SLT tex.w,1,tex.w; \n\
       ### modulate with texture color \n\
       MUL col,col,tex; \n\
       ### apply pattern to opacity \n\
@@ -988,6 +991,7 @@ void minisurf::enablepattern(float ambient,
       MOV crd,fragment.texcoord[3]; \n\
       ### fetch actual texel \n\
       TEX tex,fragment.texcoord[0],texture[0],3D; \n\
+      SLT tex.w,1,tex.w; \n\
       ### modulate with texture color \n\
       MUL col,col,tex; \n\
       ### apply pattern to opacity \n\
