1190014e55adf955da24678c9de24bf98fb07adc
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2013-02-20 16:47:37 +0000

    fix simple viewer example

--- libmini/qtviewer/simple.cmake/qtsimple.cpp
+++ libmini/qtviewer/simple.cmake/qtsimple.cpp
@@ -3,49 +3,7 @@
 #include <QtGui/QApplication>
 #include <QtOpenGL/qgl.h>
 
-#include "qtsimple.h"
-
-class MyQtViewer: public QtSimpleViewer
-{
-public:
-
-   MyQtViewer()
-   {
-      QAction *quitAction;
-
-      quitAction = new QAction(tr("Q&uit"), this);
-      quitAction->setShortcuts(QKeySequence::Quit);
-      connect(quitAction, SIGNAL(triggered()), this, SLOT(close()));
-   }
-
-   ~MyQtViewer()
-   {}
-
-protected:
-
-   virtual mininode_group *build_ecef_geometry()
-   {
-      mininode_group *group = new mininode_group();
-
-      // define equator:
-
-      minidyna<miniv3d> pos;
-      static const int eqlines = 500;
-
-      for (int i=0; i<=eqlines; i++)
-      {
-         minicoord c(miniv3d((double)i/eqlines*360*3600, 0.0, 0.0), minicoord::MINICOORD_LLH);
-         c.convert2(minicoord::MINICOORD_ECEF);
-         pos.append(c.vec);
-      }
-
-      group->append_child(new mininode_color(miniv3d(0.5, 0.5, 0.5)))->
-         append_child(new mininode_geometry_band(pos, pos, 20000));
-
-      return(group);
-   }
-
-};
+#include "viewer.h"
 
 int main(int argc, char *argv[])
 {
---
+++ libmini/qtviewer/simple.cmake/viewer.h
@@ -0,0 +1,22 @@
+// (c) by Stefan Roettger, licensed under GPL 2+
+
+#ifndef VIEWER_H
+#define VIEWER_H
+
+#include "qtsimpleviewer.h"
+
+class MyQtViewer: public QtSimpleViewer
+{
+   Q_OBJECT;
+
+public:
+
+   MyQtViewer();
+   ~MyQtViewer();
+
+protected:
+
+   virtual mininode_group *build_ecef_geometry();
+};
+
+#endif
