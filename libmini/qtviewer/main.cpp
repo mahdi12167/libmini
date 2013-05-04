973fcd1b58211264a14d9bfec477df4cc0901946
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2013-05-04 09:08:11 +0000

    log warnings and errors

--- libmini/qtviewer/main.cpp
+++ libmini/qtviewer/main.cpp
@@ -13,18 +13,20 @@
 
 void errormsg(const char *file,int line,int fatal)
 {
-   QString msg;
+   ministring msg;
 
    if (fatal==MINI_ERROR_NONFATAL) msg = "warning";
    else if (fatal==MINI_ERROR_MEM) msg = "insufficient memory";
    else if (fatal==MINI_ERROR_IO) msg = "io error";
    else msg = "fatal error";
-   msg += " in <" + QString(file) + "> at line " + QString::number(line) + "!\n";
+   msg += ministring(" in <") + file + "> at line " + line + "!";
 
-   fprintf(stderr, "%s", msg.toStdString().c_str());
+   fprintf(stderr, "%s\n", msg.c_str());
+
+   MINILOG(msg);
 
    if (fatal!=MINI_ERROR_NONFATAL)
-      QMessageBox::warning(0, "error", msg, QMessageBox::Ok);
+      QMessageBox::warning(0, "error", msg.c_str(), QMessageBox::Ok);
 }
 
 void centerWidgetOnScreen(QWidget *widget)
@@ -51,6 +53,7 @@ int main(int argc, char *argv[])
    miniseed();
 
    minilog::on();
+   minilog::logfile("qtviewer.log");
    MINILOG("program start");
 
    MainWindow window;
