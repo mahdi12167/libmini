9b22f23f19544e14da0f1ec80d91836ed15c5810
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2013-09-06 17:25:26 +0000

    add serialization to special teapot node

--- libmini/mini/mininode_teapot.h
+++ libmini/mini/mininode_teapot.h
@@ -14,6 +14,23 @@ class mininode_geometry_teapot: public mininode_geom
 
    virtual void rendergeo(int wocolor=0,int wonrm=0,int wotex=0);
    virtual void getbbox(miniv3d &bboxmin,miniv3d &bboxmax) const;
+
+   //! serialize node to string
+   virtual ministring to_string() const
+      {return("mininode_geometry_teapot");}
+
+   //! deserialize node from string
+   virtual BOOLINT from_string(ministring &info)
+      {
+      if (info.startswith("mininode_geometry_teapot"))
+         {
+         info=info.tail("mininode_geometry_teapot");
+         return(TRUE);
+         }
+
+      return(FALSE);
+      }
+
    };
 
 #endif
