53437d8f82e3799cba1a1efad43dd37c4c0cd653
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2013-10-02 13:20:48 +0000

    add raw2iso tool

---
+++ libmini/mini/tools/raw2iso.cpp
@@ -0,0 +1,31 @@
+// (c) by Stefan Roettger, licensed under LGPL 2.1
+
+#include <mini/minibase.h>
+
+#include <mini/rawbase.h>
+#include <mini/rekbase.h>
+
+int main(int argc,char *argv[])
+   {
+   double isovalue=0.5;
+
+   char *outname;
+
+   if (argc!=3 && argc!=4)
+      {
+      printf("usage: %s <volume.raw> <iso.ply> [<iso value>]\n",argv[0]);
+      printf(" convert volume data (raw or rek format) into an iso surface (ply format)\n");
+      exit(1);
+      }
+
+   if (argc==4)
+      if (sscanf(argv[3],"%lg",&isovalue)!=1) exit(1);
+
+   outname=extractRAWvolume(argv[1],argv[2],isovalue);
+   if (outname==NULL) outname=extractREKvolume(argv[1],argv[2],isovalue);
+
+   if (outname==NULL) exit(1);
+   else free(outname);
+
+   return(0);
+   }
