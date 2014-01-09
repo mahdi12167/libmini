de5b8af68e82b2baae25554113ec45509b3d1a68
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-01-09 11:08:51 +0000

    update readme

--- libmini/qtviewer/mainconst.h
+++ libmini/qtviewer/mainconst.h
@@ -4,7 +4,7 @@
 #define MAINCONST_H
 
 #define VIEWER_NAME "QTViewer"
-#define VIEWER_VERSION "v1.9.8 as of 7.January.2014"
+#define VIEWER_VERSION "v2.0 as of 8.January.2014"
 
 #define VIEWER_LICENSE "licensed under GPL 3.0"
 #define VIEWER_COPYRIGHT "(c) by Stefan Roettger 2012-2014"
