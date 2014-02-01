24de8addcd0c35de5c89f4d2e27c3d52678f7273
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-02-01 12:33:55 +0000

    exit if installation was unsuccessful

--- libmini/qtviewer/install.sh
+++ libmini/qtviewer/install.sh
@@ -3,12 +3,17 @@
 
 echo compiling libmini...
 (cd mini; cmake -DBUILD_MINI_SFX=ON . && make -j 4)
+if ($? != 0) exit($?)
 echo compiling libgrid...
 (cd grid; cmake . && make -j 4)
+if ($? != 0) exit($?)
 echo compiling libsquish...
 (cd squish; cmake . && make -j 4)
+if ($? != 0) exit($?)
 echo compiling qtviewer...
 cmake . && make -j 4
+if ($? != 0) exit($?)
 echo installing qtviewer...
 echo enter root password when prompted
 sudo make install
+if ($? != 0) exit($?)
