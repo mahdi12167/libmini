bb60c9b080890a603984e13534c645b7b15ffcc1
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-03-24 12:00:54 +0000

    add includes

--- libmini/mini/mini_object.h
+++ libmini/mini/mini_object.h
@@ -6,11 +6,13 @@
 #include <mini/minilayer.h>
 #include <mini/miniterrain.h>
 #include <mini/miniearth.h>
+#include <mini/minipath.h>
 
 #include <mini/minicam.h>
 #include <mini/mininode.h>
 #include <mini/mininodes.h>
 #include <mini/mininode_geom.h>
+#include <mini/mininode_path.h>
 #include <mini/mininode_teapot.h>
 
 #endif
