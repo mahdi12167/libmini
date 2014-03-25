cec1fb3f7e6d5bf5f6f2bb4cdce1d0f55ca71957
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-03-25 06:43:02 +0000

    first build libsquish

--- libmini/qtviewer/install.sh
+++ libmini/qtviewer/install.sh
@@ -1,14 +1,14 @@
 #!/bin/tcsh -f
 # unix install script
 
+echo compiling libsquish...
+(cd squish; cmake . && make -j 4)
+if ($? != 0) exit($?)
 echo compiling libmini...
 (cd mini; cmake -DBUILD_MINI_SFX=ON . && make -j 4)
 if ($? != 0) exit($?)
 echo compiling libgrid...
 (cd grid; cmake . && make -j 4)
-if ($? != 0) exit($?)
-echo compiling libsquish...
-(cd squish; cmake . && make -j 4)
 if ($? != 0) exit($?)
 echo compiling qtviewer...
 cmake . && make -j 4
