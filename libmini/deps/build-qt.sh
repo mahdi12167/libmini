f8252564698ea84aa3613f972d59132485ec7138
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-03-25 10:48:37 +0000

    do not recommend qt 4.8

--- libmini/deps/build-qt.sh
+++ libmini/deps/build-qt.sh
@@ -3,7 +3,7 @@
 # this script pulls and installs Qt from source
 # installation directory is /usr/local/Trolltech/...
 
-set qt4=http://download.qt-project.org/official_releases/qt/4.8/4.8.5/qt-everywhere-opensource-src-4.8.5.tar.gz
+set qt4=ftp://ftp.qt.nokia.com/qt/source/qt-everywhere-opensource-src-4.7.4.tar.gz
 set qt5=http://download.qt-project.org/official_releases/qt/5.2/5.2.1/single/qt-everywhere-opensource-src-5.2.1.tar.gz
 
 set qt=$qt4
