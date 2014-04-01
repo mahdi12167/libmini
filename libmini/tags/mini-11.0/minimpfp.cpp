d08efea405a589b6d55c1f378d4daa8024b878bb
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-04-01 09:25:54 +0000

    delete old tags

--- libmini/tags/mini-11.0/minimpfp.cpp
+++
@@ -1,7 +0,0 @@
-// (c) by Stefan Roettger, licensed under LGPL 2.1
-
-#include "minimpfp.h"
-
-BOOLINT minimpfp_base::MINIMPFP_OVERFLOW=FALSE;
-BOOLINT minimpfp_base::MINIMPFP_DIVBYZERO=FALSE;
-BOOLINT minimpfp_base::MINIMPFP_NEGSQROOT=FALSE;
