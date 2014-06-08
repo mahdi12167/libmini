d57d41a3ee2544bcc24ff1d4265a986d45835dfe
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-06-08 20:52:26 +0000

    update wget for qt 5.3.0

--- libmini/deps/build-qt.sh
+++ libmini/deps/build-qt.sh
@@ -4,7 +4,7 @@
 # installation directory is /usr/local/Trolltech/...
 
 set qt4=ftp://ftp.qt.nokia.com/qt/source/qt-everywhere-opensource-src-4.7.4.tar.gz
-set qt5=http://download.qt-project.org/official_releases/qt/5.2/5.2.1/single/qt-everywhere-opensource-src-5.2.1.tar.gz
+set qt5=http://download.qt-project.org/official_releases/qt/5.3/5.3.0/single/qt-everywhere-opensource-src-5.3.0.tar.gz
 
 set qt=$qt4
 
