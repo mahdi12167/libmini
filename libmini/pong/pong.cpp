60f7c2b8ff84adbc57ce26752951b61970a614b8
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-06-08 19:21:52 +0000

    tweak

--- libmini/pong/pong.cpp
+++ libmini/pong/pong.cpp
@@ -5,13 +5,17 @@ int main(int argc, char** argv)
 {
    QApplication app(argc, argv);
 
-   if (argc<=1)
+   if (argc <= 1)
    {
+      std::cout << "starting server" << std::endl;
+
       SSLServer server;
       server.start("cert.pem", "key.pem", 10000);
    }
    else
    {
+      std::cout << "starting client" << std::endl;
+
       SSLClient client;
       client.start(argv[1], 10000);
    }
