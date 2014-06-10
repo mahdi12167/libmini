b2f239d2cdeea459699117fd55f4ee8724b3a1e8
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-06-10 12:03:59 +0000

    add test app

---
+++ libmini/pong/test.cpp
@@ -0,0 +1,26 @@
+// (c) by Stefan Roettger, licensed under GPL 3.0
+
+#include <QApplication>
+
+#include "sslsocket.h"
+
+int main(int argc, char **argv)
+{
+   QApplication app(argc, argv);
+
+   if (argc <= 1)
+   {
+      SSLTestServerConnectionFactory factory;
+      SSLServer server(&factory);
+      server.start("cert.pem", "key.pem", 10000);
+
+      return(app.exec());
+   }
+   else
+   {
+      SSLTestClient client;
+      client.start(argv[1], 10000, false);
+   }
+
+   return(0);
+}
---
+++ libmini/pong/test.pro
@@ -0,0 +1,7 @@
+TARGET = test
+TEMPLATE = app
+
+QT += core gui network
+
+HEADERS += sslsocket.h
+SOURCES += test.cpp sslsocket.cpp
