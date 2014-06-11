006b84e7837cff107a552ca3f3f387407fff72fd
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-06-11 14:30:53 +0000

    send data block size as header

--- libmini/pong/ssltransmission.cpp
+++ libmini/pong/ssltransmission.cpp
@@ -39,8 +39,22 @@ SSLTransmissionServerConnection::SSLTransmissionServerConnection(int socketDescr
 // start reading from an established connection
 void SSLTransmissionServerConnection::startReading(QSslSocket *socket)
 {
+   qint64 block;
+
+   QDataStream in(socket);
+   in.setVersion(QDataStream::Qt_4_0);
+
+   // check if header has arrived
+   if (socket->bytesAvailable() < (int)sizeof(block)) return;
+
+   // read data block size
+   in >> block;
+
+   // check if entire data block has arrived
+   if (socket->bytesAvailable() < block) return;
+
    // read data from the ssl socket
-   QByteArray data = socket->readAll();
+   QByteArray data = socket->read(block);
 
    // signal transmission of data chunk
    emit transmit(data);
@@ -53,6 +67,16 @@ SSLTransmissionClient::SSLTransmissionClient(QByteArray &data, QObject *parent)
 // start writing through an established connection
 void SSLTransmissionClient::startWriting(QSslSocket *socket)
 {
-   // write data to the ssl socket
+   QByteArray header;
+   QDataStream out(&header, QIODevice::WriteOnly);
+
+   // assemble header to contain data block length
+   out.setVersion(QDataStream::Qt_4_0);
+   out << (qint64)data_.size();
+
+   // write header to the ssl socket
+   socket->write(header, header.size());
+
+   // write data block to the ssl socket
    socket->write(data_, data_.size());
 }
