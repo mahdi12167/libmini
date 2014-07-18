91b9fa96c6ee65ae73147eecb2cfe7d068160231
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-07-18 11:45:34 +0000

    remove old tags

--- libmini/tags/mini-11.1/example.cmake/module.h
+++
@@ -1,8 +0,0 @@
-// (c) by Stefan Roettger, licensed under LGPL 2.1
-
-#ifndef MODULE_H
-#define MODULE_H
-
-void show_example(int argc,char *argv[]);
-
-#endif
--- libmini/tags/mini-11.3/build.bat
+++
@@ -1,2 +0,0 @@
-cmake -G "NMake Makefiles" -DBUILD_MINI_SFX=ON -DCMAKE_BUILD_TYPE=RELEASE
-nmake
