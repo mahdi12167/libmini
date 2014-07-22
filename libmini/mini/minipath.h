a77897dcb4f1d8b4bb80bec16f7747b357cef2ec
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-07-22 06:59:53 +0000

    fix declaration

--- libmini/mini/minipath.h
+++ libmini/mini/minipath.h
@@ -19,7 +19,7 @@ class minipath : public minicurve
    //! constructor
    minipath(ministring filename,
             double start=0.0,double stop=1.0,
-            double max_length=50000.0,double min_accuracy=50.0,max_accel=2.0)
+            double max_length=50000.0,double min_accuracy=50.0,double max_accel=2.0)
       : minicurve(start,stop),
         name("path"), activity("none"), description("created by libmini")
       {
