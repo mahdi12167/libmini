825e5c5d5f6cbf8ac1cf110ebc832db83a259fb5
stefan@stereofx.org <stefan@stereofx.org@1f9b7ce6-213e-0410-97bc-ef2f4dd8f60f>
2014-10-25 15:39:26 +0000

    also tabify .c files

--- libmini/mini/tabify.sh
+++ libmini/mini/tabify.sh
@@ -10,7 +10,7 @@ if ("$option" == "") then
    echo " replaces all tabs in <file> with 4-space (-x) or 8-space (-X)"
    echo " or replaces all tabs at the beginning of each line with 4-space (-u) or 8-space (-U) and the other way round (-t|T)"
    echo " additionally removes trailing white space, all spaces before a tab, and gets rid of all ctrl-m"
-   echo " whithout a specified file argument all .cpp and .h files are treated accordingly"
+   echo " whithout a specified file argument all .c/.cpp and .h files are treated accordingly"
    exit
 endif
 
@@ -42,22 +42,22 @@ set rmctrlm="s/$ctrlm//"
 
 if ($file == "") then
    if ($tabify == 1) then
-      find . \( -name \*.cpp -o -name \*.h \) -exec $app -x {} \;
+      find . \( -name \*.c -o -name \*.cpp -o -name \*.h \) -exec $app -x {} \;
    endif
    if ($tabify == 2) then
-      find . \( -name \*.cpp -o -name \*.h \) -exec $app -t {} \; -exec $app -u {} \;
+      find . \( -name \*.c -o -name \*.cpp -o -name \*.h \) -exec $app -t {} \; -exec $app -u {} \;
    endif
    if ($tabify == 3) then
-      find . \( -name \*.cpp -o -name \*.h \) -exec $app -u {} \; -exec $app -t {} \;
+      find . \( -name \*.c -o -name \*.cpp -o -name \*.h \) -exec $app -u {} \; -exec $app -t {} \;
    endif
    if ($tabify == 4) then
-      find . \( -name \*.cpp -o -name \*.h \) -exec $app -X {} \;
+      find . \( -name \*.c -o -name \*.cpp -o -name \*.h \) -exec $app -X {} \;
    endif
    if ($tabify == 5) then
-      find . \( -name \*.cpp -o -name \*.h \) -exec $app -T {} \; -exec $app -U {} \;
+      find . \( -name \*.c -o -name \*.cpp -o -name \*.h \) -exec $app -T {} \; -exec $app -U {} \;
    endif
    if ($tabify == 6) then
-      find . \( -name \*.cpp -o -name \*.h \) -exec $app -U {} \; -exec $app -T {} \;
+      find . \( -name \*.c -o -name \*.cpp -o -name \*.h \) -exec $app -U {} \; -exec $app -T {} \;
    endif
 else
    if ($tabify == 1) then
